6740eedede0335fa032fb2f75e2c1eb33401fe9e
rolando guerra <rolandoguerra1991@gmail.com>
2022-11-09 19:23:47 -0400

    vista de ajustes

--- src/views/profile/Settings.vue
+++ src/views/profile/Settings.vue
@@ -1,11 +1,87 @@
 <template>
-    
+    <div class="pt-5">
+        <h1 class="text-2xl">Cambiar contraseña</h1>
+    </div>
+    <div class="grid">
+        <div class="col-6">
+            <div class="border-1 border-gray-300 p-4 border-round">
+                <div class="field">
+                    <label>Escribe te contraseña actual</label>
+                    <div class="p-inputgroup">
+                        <InputText :type="inputPasswordType" v-model="currentPassword" />
+                        <span class="p-inputgroup-addon cursor-pointer" @click="toggleShowPassword">
+                            <i :class="eyeIconState"></i>
+                        </span>
+                    </div>
+                </div>
+                <div class="field">
+                    <label>Nueva contraseña</label>
+                    <div class="p-inputgroup">
+                        <InputText :type="inputPasswordType" v-model="newPassword" />
+                        <span class="p-inputgroup-addon cursor-pointer" @click="toggleShowPassword">
+                            <i :class="eyeIconState"></i>
+                        </span>
+                    </div>
+                </div>
+                <div class="field">
+                    <label>Confirmar nueva contraseña</label>
+                    <div class="p-inputgroup">
+                        <InputText :type="inputPasswordType" v-model="confirmNewPassword" />
+                        <span class="p-inputgroup-addon cursor-pointer" @click="toggleShowPassword">
+                            <i :class="eyeIconState"></i>
+                        </span>
+                    </div>
+                </div>
+                <div class="field flex justify-content-end">
+                    <Button label="Guardar" class="px-5"/>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="grid">
+        <div class="col-6">
+            <div class="border-1 border-gray-300 p-4 border-round">
+                <p>Cambio de idioma</p>
+                <SelectButton v-model="languageSelected" :options="languages" aria-labelledby="single" />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const currentPassword = ref<string>('');
+const newPassword = ref<string>('');
+const confirmNewPassword = ref<string>('');
+const showPassword = ref<boolean>(false);
+const languages = ref<string[]>(['EN', 'ESP']);
+const languageSelected = ref<string>('ESP');
+
+const toggleShowPassword = () => {
+    showPassword.value = !showPassword.value
+}
+
+const eyeIconState = computed(() => showPassword.value ? 'pi pi-eye-slash' : 'pi pi-eye');
+const inputPasswordType = computed(() => showPassword.value ? 'text' : 'password');
 
 </script>
 
 <style lang="css" scoped>
-
+.p-button, .p-button:hover {
+    background: var(--primary-color);
+    border: 1px solid var(--primary-color);
+}
+.p-selectbutton .p-button.p-highlight {
+    background: #E7E6E7;
+    border-color: #E7E6E7;
+    color: #1B1B19;
+    font-weight: 600;
+}
+.p-selectbutton .p-button.p-highlight:hover {
+    background: #E7E6E7;
+    border-color: #E7E6E7;
+    color: #1B1B19;
+    font-weight: 600;
+}
 </style>
