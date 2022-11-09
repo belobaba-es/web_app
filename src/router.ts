33ac06f18d02296e1209ff94d32dc44a247659bf
rolando guerra <rolandoguerra1991@gmail.com>
2022-11-08 21:17:29 -0400

    iniciando desarrollo en la vista de perfil

--- src/router.ts
+++ src/router.ts
@@ -2,12 +2,20 @@ import {createRouter, createWebHistory} from "vue-router";
 import Home from './views/Home.vue'
 import Contact from './views/Contact.vue'
 import Dashboard from './views/dashboard/Dashboard.vue'
+import Profile from './views/profile/Profile.vue'
+
 export default createRouter({
     history: createWebHistory(),
     routes: [
         {
             path: '/',
             component: Dashboard,
+            children: [
+                {
+                    path: '/profile',
+                    component: Profile,
+                },
+            ]
         },
         {
             path: '/contact',
