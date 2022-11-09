dbf4eb1dee136b97f273b0f8a2e423b8d2105b8a
manuel <manuel.marino03@gmail.com>
2022-11-08 21:12:01 -0400

    cambios en el dashboard y fuentes

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="layout-wrapper" @click="onMenuToggle_2">
-         <AppTopBar />
+	<div :class="containerClass" @click="onWrapperClick">
+         <AppTopBar  @menu-toggle="onMenuToggle"/>
         <div class="layout-sidebar" @click="onSidebarClick">
             <AppMenu :model="menu" @menuitem-click="onMenuItemClick" />
         </div> 
@@ -11,21 +11,18 @@
             </div>
         </div>
 
-		
         <transition name="layout-mask">
             <div class="layout-mask p-component-overlay" v-if="mobileMenuActive"></div>
         </transition>
 	</div>
-    <div>
-        asdasd
-    </div>
+    
 </template>
 
 <script lang="ts">
 import AppTopBar from './AppTopbar.vue';
 import AppMenu from './AppMenu.vue';
 
-import { defineComponent } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
 
 export default defineComponent({
   components: {
@@ -33,28 +30,20 @@ export default defineComponent({
     'AppMenu': AppMenu,
   },
   props: {
-
   },
   data() {
         return {
-            layoutMode: 'overlay',
             staticMenuInactive: false,
-            overlayMenuActive: false,
-            mobileMenuActive: false,
+            example:true,
             menu : [
                 {
-                    label: 'Home',
-                    items: [{
-                        label: 'Conciliaciones', icon: 'pi pi-fw pi-home', to: '/'
-                    }]
-         
-
-                },
-                {
-                    label: 'Configuración',
+                    label: '',
                     items: [
-                        {label: 'Empresa', icon: 'pi pi-fw pi-building', to: '/company',},
-                        {label: 'Categoria', icon: 'pi pi-fw pi-tags', to: '/category'}
+                        {label: 'Inicio', class: 'icon-home', icon: 'pi', to: '/'},
+                        {label: 'Depositos', class: 'icon-piggy-bank', icon: 'pi', to: '/',},
+                        {label: 'Retiros', class: 'icon-bank', icon: 'pi', to: '/'},
+                        {label: 'Billetera', class:'icon-wallet', icon: 'pi', to: '/'},
+                        {label: 'Intercambio', class: 'icon-swap', icon: 'pi', to: '/'},
                     ]
                     
                 },
@@ -62,11 +51,23 @@ export default defineComponent({
         }
     },
     setup (props,{emit}) {
-        const onMenuToggle_2 = (event: Event) => {
-        // `event` implicitly has `any` type
-            console.log((event.target as HTMLInputElement).value)
+        const mobileMenuActive = ref(false);
+        const overlayMenuActive = ref(false)
+        const staticMenuInactive = ref(false)
+        const containerClass = computed(()=>
+
+            {
+                return ['layout-wrapper', 'layout-static', {
+                    'layout-static-sidebar-inactive': !staticMenuInactive.value,
+                    'layout-mobile-sidebar-active': mobileMenuActive.value,
+                }];
+            }
+        ) 
+        
+        const onMenuToggle = () =>{
+            staticMenuInactive.value = !staticMenuInactive.value
+            console.log('menu',staticMenuInactive.value)
         }
-       
         const onWrapperClick = ()=> {
 
         }
@@ -75,154 +76,29 @@ export default defineComponent({
         }
 
         const onMenuItemClick = (event:Event) =>{
+
             if (event.item && !event.item.items) {
+                
                 // this.overlayMenuActive = false;
                 // this.mobileMenuActive = false;
             }
         }
+
+        
         return {
-            onMenuToggle_2,
+            containerClass,
             onWrapperClick,
             onSidebarClick,
-            onMenuItemClick
+            onMenuItemClick,
+            mobileMenuActive,
+            overlayMenuActive,
+            staticMenuInactive,
+            onMenuToggle
         }
     },
   
 })
 
-
-// export default {
-//     components: {
-//         'AppTopBar': AppTopBar,
-//         'AppMenu': AppMenu,
-       
-//     },
-//     emits: ['change-theme'],
-//     data() {
-//         return {
-//             layoutMode: 'overlay',
-//             staticMenuInactive: false,
-//             overlayMenuActive: false,
-//             mobileMenuActive: false,
-//             menu : [
-//                 {
-//                     label: 'Home',
-//                     items: [{
-//                         label: 'Conciliaciones', icon: 'pi pi-fw pi-home', to: '/'
-//                     }]
-         
-
-//                 },
-//                 {
-//                     label: 'Configuración',
-//                     items: [
-//                         {label: 'Empresa', icon: 'pi pi-fw pi-building', to: '/company',},
-//                         {label: 'Categoria', icon: 'pi pi-fw pi-tags', to: '/category'}
-//                     ]
-                    
-//                 },
-//             ]
-//         }
-//     },
-//     watch: {
-//         $route() {
-//             this.menuActive = false;
-//             this.$toast.removeAllGroups();
-//         }
-//     },
-//     methods: {
-//         onWrapperClick() {
-//             if (!this.menuClick) {
-//                 this.overlayMenuActive = false;
-//                 this.mobileMenuActive = false;
-//             }
-
-//             this.menuClick = false;
-//         },
-//         onMenuToggle() {
-//             this.menuClick = true;
-
-//             if (this.isDesktop()) {
-//                 if (this.layoutMode === 'overlay') {
-// 					if(this.mobileMenuActive === true) {
-// 						this.overlayMenuActive = true;
-// 					}
-
-//                     this.overlayMenuActive = !this.overlayMenuActive;
-// 					this.mobileMenuActive = false;
-//                 }
-//                 else if (this.layoutMode === 'static') {
-//                     this.staticMenuInactive = !this.staticMenuInactive;
-//                 }
-//             }
-//             else {
-//                 this.mobileMenuActive = !this.mobileMenuActive;
-//             }
-
-//             event.preventDefault();
-//         },
-//         onSidebarClick() {
-//             this.menuClick = true;
-//         },
-//         onMenuItemClick(event) {
-//             if (event.item && !event.item.items) {
-//                 this.overlayMenuActive = false;
-//                 this.mobileMenuActive = false;
-//             }
-//         },
-// 		onLayoutChange(layoutMode) {
-// 			this.layoutMode = layoutMode;
-// 		},
-//         addClass(element, className) {
-//             if (element.classList)
-//                 element.classList.add(className);
-//             else
-//                 element.className += ' ' + className;
-//         },
-//         removeClass(element, className) {
-//             if (element.classList)
-//                 element.classList.remove(className);
-//             else
-//                 element.className = element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-//         },
-//         isDesktop() {
-//             return window.innerWidth >= 992;
-//         },
-//         isSidebarVisible() {
-//             if (this.isDesktop()) {
-//                 if (this.layoutMode === 'static')
-//                     return !this.staticMenuInactive;
-//                 else if (this.layoutMode === 'overlay')
-//                     return this.overlayMenuActive;
-//             }
-
-//             return true;
-//         }
-//     },
-//     computed: {
-//         containerClass() {
-//             return ['layout-wrapper', {
-//                 'layout-overlay': this.layoutMode === 'overlay',
-//                 'layout-static': this.layoutMode === 'static',
-//                 'layout-static-sidebar-inactive': this.staticMenuInactive && this.layoutMode === 'static',
-//                 'layout-overlay-sidebar-active': this.overlayMenuActive && this.layoutMode === 'overlay',
-//                 'layout-mobile-sidebar-active': this.mobileMenuActive,
-// 				'p-input-filled': this.$primevue.config.inputStyle === 'filled',
-// 				'p-ripple-disabled': this.$primevue.config.ripple === false
-//             }];
-//         },
-//         logo() {
-//             return (this.$appState.darkTheme) ? "images/logo-white.svg" : "images/logo.svg";
-//         }
-//     },
-//     beforeUpdate() {
-//         if (this.mobileMenuActive)
-//             this.addClass(document.body, 'body-overflow-hidden');
-//         else
-//             this.removeClass(document.body, 'body-overflow-hidden');
-//     },
-    
-// }
 </script>
 
 <style >
