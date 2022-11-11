c1be85bafb00bb5570897a15c7d912130e429dd7
rolando guerra <rolandoguerra1991@gmail.com>
2022-11-10 20:56:36 -0400

    comenzando vista de documentos

--- src/views/profile/Documents.vue
+++ src/views/profile/Documents.vue
@@ -1,9 +1,31 @@
 <template>
-    
+    <div class="pt-5">
+        <h1 class="text-2xl">Documentos</h1>
+    </div>
+    <div class="border-2 border-200 border-solid p-3">
+        <div class="grid">
+            <div class="col-2">
+                <div class="p-5 flex-column border-2 border-200 border-solid surface-100 cursor-pointer">
+                    <div class="flex justify-content-center mb-3">
+                        <img src="../../assets/icons/icon-document.svg" alt="" srcset="">
+                    </div>
+                    <div class="flex justify-content-center">
+                        <span>Archivo_1.jpg</span>
+                    </div>
+                    <div class="flex justify-content-center">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="mt-5">
+        <Button label="Actualizar Documentos" />
+    </div>
 </template>
 
 <script setup lang="ts">
-
+import Button from 'primevue/button';
 </script>
 
 <style scoped>
