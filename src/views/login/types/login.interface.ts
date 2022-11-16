0b78dd937e24a03aeb853a9f68d60526243ec16a
manuel <manuel.marino03@gmail.com>
2022-11-16 11:15:53 -0400

    login interface

---
+++ src/views/login/types/login.interface.ts
@@ -0,0 +1,36 @@
+
+export interface LoginResponse {
+    message: string;
+    data:    LoginData;
+}
+
+export interface LoginData {
+    active:       boolean;
+    accountId:    string;
+    email:        string;
+    token:        string;
+    country:      string;
+    postalCode:   string;
+    region:       string;
+    middleName:   string;
+    firstName:    string;
+    contactId:    string;
+    phoneCountry: string;
+    city:         string;
+    taxId:        string;
+    label:        string;
+    streetOne:    string;
+    phoneNumber:  string;
+    phoneSMS:     boolean;
+    contactType:  string;
+    lastName:     string;
+    dateBirth:    string;
+    taxCountry:   string;
+    streetTwo:    string;
+    account:      Account;
+}
+
+export interface Account {
+    accountId: string;
+    status:    string;
+}
