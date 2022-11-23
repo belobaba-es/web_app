315d1122785e233d04b20cc78c349404a744c8c7
Manuel <manuel.marino03@gmail.com>
2022-11-23 17:50:30 -0400

    logica para virtual scroll

--- src/views/deposit/Crypto.vue
+++ src/views/deposit/Crypto.vue
@@ -1,3 +1,4 @@
+
 <template>
     <div>
         <NewWallet :assets="assets"  v-model:display="displayNew" v-model:asset-select="assetSelect" @create="onCreateAddress"> </NewWallet>
@@ -6,7 +7,7 @@
         <p class="text-3xl font-medium mb-4">{{t('deposit')}} / <span class="text-primary">{{t('crypto')}} </span></p>
         
         <div class="grid">
-            <div class="col-10 ">
+            <div class="col-8 lg:col-6">
                 
                 <span class="p-input-icon-left flex p-fluid">
                     <i class="pi pi-search" />
@@ -14,11 +15,23 @@
                     <Button class="p-button search-btn" :label="t('search')"/>
                 </span>
             </div>
-            <div class="col-4">
+            <div class="col-4 lg:col-4">
                 <Button class="p-button" :label="t('newWallet')" @click="displayNew = !displayNew"/>
             </div>
-            <div class="col-10">.
-                <ListAssets :assets="assets" :payment-address="paymentAddress" v-on:select="viewAddressAsset($event)"></ListAssets>
+            <div class="col-12 lg:col-10 ">.
+                <VirtualScroller  scrollHeight="500px"  :items="paymentAddress" :itemSize="75" showLoader :loading="lazyLoading"  :lazy=true  @lazy-load="onLazyLoad">
+                    <template v-slot:item="{ item, options }">
+                        <div :class="['scroll-item pr-2', {'odd': options.odd}]" >
+                            <AssetDetail :assets="assets" :payment-address="item" @select="viewAddressAsset($event)"></AssetDetail>
+                        </div>
+                    </template>
+                    <template v-slot:loader="{ options }">
+                        <div :class="['scroll-item p-2', {'odd': options.odd}]" style="height: 50px" >
+                            <Skeleton :width="options.even ? '60%' : '50%'" height="1.3rem" />
+                        </div>
+                    
+                    </template>
+                </VirtualScroller>
                 
             </div>
             <div class="col-4">
@@ -28,23 +41,28 @@
     </div>
 
 </template>
-
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue';
 import { useI18n } from 'vue-i18n'
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
-
+import VirtualScroller from 'primevue/virtualscroller';
 import { Asset, CreatePaymentAddress, EventCreatePaymentAddress, PaymentAddress } from './types/asset.interface';
 import NewWallet from './components/NewWallet.vue'
 import ViewAddress from './components/ViewAddress.vue';
 import { AssetsService } from './services/assets';
-import ListAssets from './components/ListAssets.vue';
+import AssetDetail from './components/AssetDetail.vue';
+import Skeleton from 'primevue/skeleton';
+
+
 
 const { t } = useI18n({ useScope: 'global' })
-
+const nextPag = ref('')
 const display = ref(false);
 const displayNew = ref(false);
+const lazyItems = ref(Array.from({ length: 10000 }));
+const lazyLoading = ref(false);
+const loadLazyTimeout = ref(null);
 const assetSelect = ref(null)
 const selectViewAsset= ref<Asset | null > (null);
 const selectPaymentAddress = ref<PaymentAddress | null> (null);
@@ -71,7 +89,10 @@ const paymentAddress = ref<PaymentAddress[]>([])
 
 onMounted(async () => {
     assetsService.list().then(data => assets.value = data);
-    assetsService.listPaymentAddress().then(data=>paymentAddress.value = data.results)
+    assetsService.listPaymentAddress().then(data=>{
+        paymentAddress.value = data.results;
+        nextPag.value = data.nextPag
+    })
 });
 
 const onCreateAddress = (event: EventCreatePaymentAddress) =>{
@@ -88,6 +109,25 @@ const onCreateAddress = (event: EventCreatePaymentAddress) =>{
         displayNew.value = false;
         display.value = true;
     })
+}
+
+const onLazyLoad = (event: any)=>{
+    const { first, last } = event;
+     
+    if( last == paymentAddress.value.length && nextPag.value != ''){
+        lazyLoading.value = true;
+        assetsService.listPaymentAddress(nextPag.value).then(data=>{
+            paymentAddress.value = [...paymentAddress.value, ...data.results]
+            nextPag.value = data.nextPag
+            
+        }).finally(()=>{
+            lazyLoading.value = false
+            
+        })
+    }
+    
+   
+    
 }
 </script>
 
