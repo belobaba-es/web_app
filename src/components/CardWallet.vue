13100eb3180cdf00bc252697f0818d6412c5b7af
abejarano <programador.angel@gmail.com>
2022-12-09 12:24:07 -0300

    [FEAT] trabajando en wallets.

--- src/components/CardWallet.vue
+++ src/components/CardWallet.vue
@@ -1,32 +1,49 @@
 <template>
 
-  <section class="flex justify-content-center mt-5">
-    <div class="col-3 card mr-2 ml-2" v-for="(item) in getWallets()">
-      <div class="grid">
-        <div class="col-4">
-          <p class="font-medium" style="font-size: 9pt">{{item.name}}</p>
-          <img :src="item.icon" width="120" />
-        </div>
-        <div class="col-8 ">
-          <p class="font-bold text-right">
-            {{calc(item.assetCode, item.balance, item.blockedBalance ?? 0)}}
-          </p>
 
+
+    <section class="flex justify-content-center mt-5">
+      <div v-if="getWallets().length === 0" class="field card col-3 mr-2 ml-2" v-for="(x) in ['','','']">
+        <div class="grid">
+          <div class="col-4">
+            <Skeleton width="5rem" class="mb-2"></Skeleton>
+            <Skeleton shape="circle" size="6rem" class="mr-2"></Skeleton>
+          </div>
+
+          <div class="col-8">
+            <div class="w-100 h-100" style="position: relative">
+              <p class="font-semi-bold text-right balance">
+                <Skeleton width="10rem" class="mb-2"></Skeleton>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
 
-    </div>
+      <div class="col-3 card mr-2 ml-2" v-for="(item) in getWallets()">
+        <div class="grid">
+          <div class="col-4">
+            <p class="font-bold text-uppercase" style="font-size: 9pt">{{ item.name }}</p>
+            <img :src="item.icon" width="120"/>
+          </div>
+          <div class="col-8">
+            <div class="w-100 h-100" style="position: relative">
+              <p class="font-semi-bold text-right balance">
+                {{ calc(item.assetCode, item.balance, item.blockedBalance ?? 0) }}
+              </p>
+            </div>
+          </div>
+        </div>
 
-  </section>
+      </div>
+
+    </section>
 </template>
 <script setup lang="ts">
-
+import Skeleton from 'primevue/skeleton';
 import {useBalanceWallet} from "../composables/useBalanceWallet";
 
 const {getAllWallets} = useBalanceWallet()
-
-// const wallets = ref<BalanceWallet[]>()
-
 
 const getWallets = () => {
   return getAllWallets()
@@ -43,5 +60,9 @@ const calc = (assetCode: string, balance: number, blocked: number) => {
 </script>
 
 <style lang="scss">
-
+.balance {
+  position: absolute;
+  bottom: 0;
+  right: 2rem;
+}
 </style>
