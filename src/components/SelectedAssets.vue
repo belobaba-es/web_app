3e2e896611649bb021d89285305e71d5d2aebbad
abejarano <programador.angel@gmail.com>
2022-12-21 07:54:18 -0300

    [FIX] css modal assets selector.

--- src/components/SelectedAssets.vue
+++ src/components/SelectedAssets.vue
@@ -19,7 +19,6 @@
       :show-modal="showModal"
       @update:visible="modal($event)"
       closeIcon="pi pi-times-circle"
-      :breakpoints="{ '960px': '75vw', '640px': '100vw' }" :style="{ width: '50vw' }"
       @selected-asset="selectedAsset"
   />
 </template>
