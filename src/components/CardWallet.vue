373a99d04b16ccd8f9cc02790942b1a8becc3409
Arturo Ruiz <gregory.arturo.ruiz@gmail.com>
2023-01-18 19:58:40 -0400

    upload files

--- src/components/CardWallet.vue
+++ src/components/CardWallet.vue
@@ -133,7 +133,7 @@ const isWallet = route.includes('wallet')
 const getWallets = () => {
   const wallets = getAllWallets()
   submitting.value = false
-  return wallets
+  return wallets.sort((a, b) => (a.assetCode === 'USD' ? -1 : b.assetCode === 'USD' ? 1 : 0));
 }
 
 const calc = (assetCode: string, balance: number, blocked: number) => {
