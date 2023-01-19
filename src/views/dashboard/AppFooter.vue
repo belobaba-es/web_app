c01f70c1afad4eb454ca7527dec472ed60f7f3e4
Arturo Ruiz <gregory.arturo.ruiz@gmail.com>
2023-01-19 13:28:55 -0400

    update

--- src/views/dashboard/AppFooter.vue
+++ src/views/dashboard/AppFooter.vue
@@ -3,8 +3,7 @@
     <div class="layout-footer hidden sm:hidden md:hidden lg:flex xl:flex mb-3">
       <div class="flex justify-content-start">
         <div class="flex align-items-center justify-content-center">
-          <span>&copy; </span>
-          <span class="text"> Noba.Cash {{ getYear() }} - All rights reserved. </span>
+          <span class="text"> Noba.Cash <sup>TM</sup> {{ getYear() }} - All rights reserved. </span>
         </div>
       </div>
     </div>
