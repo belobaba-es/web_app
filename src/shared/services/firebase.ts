2b5ce1e87cce91b7fc77b22375f59c54ef59036e
Moises Serrano <moycs777@gmail.com>
2023-01-23 20:12:51 -0300

    [FEAT] Deleting firebase cloud function

--- src/shared/services/firebase.ts
+++ src/shared/services/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { child, getDatabase, off, onValue, push, ref, set, update } from 'firebase/database'
+import { getDatabase, off, onValue, ref } from 'firebase/database'
 import { BehaviorSubject, Observable } from 'rxjs'
 import { WalletBalancesType } from '../types/walletBalanceType'
 
@@ -45,100 +45,6 @@ export class FirebaseService {
     const app = await initializeApp(config)
 
     return await getDatabase(app)
-  }
-
-  static async testWriteIntoFirebase() {
-    const db = await FirebaseService.initFirebase()
-    // await set(ref(db, `243d4551-7b49-4e12-af85-2ae442f4438c**eth`), {
-    //   accountId: 'cbbecf6b-3ede-4459-a283-c64d1276f136',
-    //   assetCode: 'ADA',
-    //   balance: 0.1,
-    //   blockedBalance: 0,
-    // })
-    //
-
-    await set(ref(db, 'cbbecf6b-3ede-4459-a283-c64d1276f136'), {
-      usd: {
-        accountId: 'cbbecf6b-3ede-4459-a283-c64d1276f136',
-        assetCode: 'USD',
-        balance: 234.22,
-        blockedBalance: 0,
-      },
-    })
-
-    await set(ref(db, '243d4551-7b49-4e12-af85-2ae442f4438c'), {
-      usd: {
-        accountId: '243d4551-7b49-4e12-af85-2ae442f4438c',
-        assetCode: 'USD',
-        balance: 451110.22,
-        blockedBalance: 0,
-      },
-      usdt: {
-        accountId: '243d4551-7b49-4e12-af85-2ae442f4438c',
-        assetCode: 'USDT',
-        balance: 1110.2,
-        blockedBalance: 0,
-      },
-      eth: {
-        accountId: '243d4551-7b49-4e12-af85-2ae442f4438c',
-        assetCode: 'ETH',
-        balance: 0.11,
-        blockedBalance: 0,
-      },
-    })
-
-    update(ref(db, '243d4551-7b49-4e12-af85-2ae442f4438c'), {
-      eth: {
-        accountId: '243d4551-7b49-4e12-af85-2ae442f4438c',
-        assetCode: 'ADA',
-        balance: 0.777,
-        blockedBalance: 0,
-      },
-    })
-
-    //
-    // await set(ref(db, 'balances' + '243d4551-7b49-4e12-af85-2ae442f4438c'), {
-    //   eth: {
-    //     accountId: 'cbbecf6b-3ede-4459-a283-c64d1276f136',
-    //     assetCode: 'ADA',
-    //     balance: 0.1,
-    //     blockedBalance: 0,
-    //   },
-    // })
-    //
-    // await set(ref(db, 'balances'), {
-    //   'cbbecf6b-3ede-4459-a283-c64d1276f136': {
-    //     eth: {
-    //       accountId: 'cbbecf6b-3ede-4459-a283-c64d1276f136',
-    //       assetCode: 'ADA',
-    //       balance: 0.1,
-    //       blockedBalance: 0,
-    //     },
-    //   },
-    // })
-
-    // blogs
-
-    // A post entry.
-    const uid = new Date()
-    const postData = {
-      author: 'username',
-      uid,
-      body: 'body',
-      title: 'title',
-      starCount: 0,
-      authorPic: 'picture',
-    }
-
-    // Get a key for a new Post.
-    const newPostKey = push(child(ref(db), 'posts')).key
-
-    // Write the new post's data simultaneously in the posts list and the user's post list.
-    const updates: any = {}
-    updates['/posts/' + newPostKey] = postData
-    updates['/user-posts/' + uid + '/' + newPostKey] = postData
-
-    update(ref(db), updates)
   }
 
   async listenFirebaseChanges() {
