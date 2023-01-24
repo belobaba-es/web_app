7b69acd72c5722c9dc0eef50c97ae354dd5f1477
abejarano <programador.angel@gmail.com>
2023-01-24 17:51:00 -0300

    [FIX] footer transparente

--- src/views/dashboard/AppFooter.vue
+++ src/views/dashboard/AppFooter.vue
@@ -1,13 +1,13 @@
 <template>
-  <footer>
-    <div class="layout-footer hidden sm:hidden md:hidden lg:flex xl:flex mb-3">
+
+    <div class="layout-footer hidden sm:hidden md:hidden lg:flex xl:flex mb-3" >
       <div class="flex justify-content-start">
         <div class="flex align-items-center justify-content-center">
           <span class="text"> Noba.Cash <sup>TM</sup> {{ getYear() }} - All rights reserved. </span>
         </div>
       </div>
     </div>
-  </footer>
+
 </template>
 
 <script lang="ts" setup>
