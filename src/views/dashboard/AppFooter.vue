ab52e888d687be62ac7c3f28b3eb7ad0f13018ef
Arturo Ruiz <gregory.arturo.ruiz@gmail.com>
2023-02-02 14:03:39 -0400

    add border top to container btn new wallet

--- src/views/dashboard/AppFooter.vue
+++ src/views/dashboard/AppFooter.vue
@@ -1,5 +1,4 @@
 <template>
-
     <div class="layout-footer hidden sm:hidden md:hidden lg:flex xl:flex" >
       <div class="flex justify-content-start">
         <div class="flex align-items-center justify-content-center">
@@ -7,7 +6,6 @@
         </div>
       </div>
     </div>
-
 </template>
 
 <script lang="ts" setup>
