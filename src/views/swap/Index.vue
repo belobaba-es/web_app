062963e3ff8fabc522da257e874e5c7e6996b4c9
abejarano <programador.angel@gmail.com>
2023-02-27 14:25:51 -0300

    Revert "add"
    This reverts commit 44734307e38e6f44bb7cdf4637fdffb5059f0c21.

--- src/views/swap/Index.vue
+++ src/views/swap/Index.vue
@@ -1,35 +1,35 @@
 <template>
   <section class="section-main">
-    <AccountValidationProcess v-show="!useUser.isAccountActive()" />
+    <AccountValidationProcess v-show="!useUser.isAccountActive()"/>
     <PageLayout :title="t('swap')" v-show="useUser.isAccountActive() && useUser.isVIP()">
       <div class="grid flex justify-content-center">
         <div class="col-12 sm:col-12 md:col-12 lg:col-6 xl:col-4">
           <div class="flex justify-content-end mb-4">
             <Button
-              type="button"
-              :label="t('viewSwapHistory')"
-              class="font-light mt-lg-5 border-300 bg-white text-black-alpha-80"
-              icon="pi pi-history"
-              icon-pos="right"
-              @click="router.push('/swap/history')"
+                type="button"
+                :label="t('viewSwapHistory')"
+                class="font-light mt-lg-5 border-300 bg-white text-black-alpha-80"
+                icon="pi pi-history"
+                icon-pos="right"
+                @click="router.push('/swap/history')"
             />
           </div>
           <div class="flex-row">
             <div>
-              <AssetInput type="fiat" v-if="transactionType === 'buy'" />
-              <AssetInput type="crypto" v-else />
+              <AssetInput type="fiat" v-if="transactionType === 'buy'"/>
+              <AssetInput type="crypto" v-else/>
             </div>
             <div class="flex justify-content-center align-items-center">
               <div
-                class="swap-circle border-circle w-5rem h-5rem m-2 flex align-items-center justify-content-center cursor-pointer"
-                @click="switchTransactionType()"
+                  class="swap-circle border-circle w-5rem h-5rem m-2 flex align-items-center justify-content-center cursor-pointer"
+                  @click="switchTransactionType()"
               >
-                <img :src="swapIcon" />
+                <img :src="swapIcon"/>
               </div>
             </div>
             <div>
-              <AssetInput type="crypto" v-if="transactionType === 'buy'" />
-              <AssetInput type="fiat" v-else />
+              <AssetInput type="crypto" v-if="transactionType === 'buy'"/>
+              <AssetInput type="fiat" v-else/>
             </div>
             <div class="flex-row justify-content-center align-items-center" v-if="progressBarPercent > 0">
               <div class="grid">
@@ -40,26 +40,26 @@
             </div>
 
             <ShowFeeBuy
-              :feeAmount="feeAmount"
-              :totalAmount="totalAmount"
-              :feeNoba="feeNoba"
-              v-if="quoteId && transactionType === 'buy'"
+                :feeAmount=feeAmount
+                :totalAmount=totalAmount
+                :feeNoba=feeNoba
+                v-if="quoteId && transactionType === 'buy'"
             />
 
             <ShowFeeSell
-              :feeAmount="feeAmount"
-              :totalAmount="totalAmount"
-              :feeNoba="feeNoba"
-              v-if="quoteId && transactionType !== 'buy'"
+                :feeAmount=feeAmount
+                :totalAmount=totalAmount
+                :feeNoba=feeNoba
+                v-if="quoteId && transactionType !== 'buy'"
             />
 
             <div class="mb-2">
               <Button
-                :label="swapBtnText"
-                class="w-full py-3 text-uppercase"
-                :disabled="loading || (!quoteId && !shouldRefreshQuote)"
-                @click="swapHandler"
-                :loading="loading"
+                  :label="swapBtnText"
+                  class="w-full py-3 text-uppercase"
+                  :disabled="loading || (!quoteId && !shouldRefreshQuote)"
+                  @click="swapHandler"
+                  :loading="loading"
               />
             </div>
             <div class="mt-3 mb-3">
@@ -69,9 +69,9 @@
         </div>
       </div>
       <ModalAssetSelector
-        :show-modal="showModalAssetSelector"
-        @update:visible="modal($event)"
-        @selected-asset="selectedAsset"
+          :show-modal="showModalAssetSelector"
+          @update:visible="modal($event)"
+          @selected-asset="selectedAsset"
       />
     </PageLayout>
   </section>
@@ -79,21 +79,21 @@
 
 <script setup lang="ts">
 import PageLayout from '../../components/PageLayout.vue'
-import { useI18n } from 'vue-i18n'
+import {useI18n} from 'vue-i18n'
 import Button from 'primevue/button'
 import AssetInput from './components/AssetInput.vue'
 import swapIcon from '../../assets/icons/swap.svg'
-import { Asset } from '../deposit/types/asset.interface'
+import {Asset} from '../deposit/types/asset.interface'
 import ModalAssetSelector from '../../components/ModalAssetSelector.vue'
 import ProgressBar from 'primevue/progressbar'
-import { useUserStore } from '../../stores/user'
-import { useRouter } from 'vue-router'
-import { useSwapStore } from '../../stores/swap'
-import { storeToRefs } from 'pinia'
+import {useUserStore} from '../../stores/user'
+import {useRouter} from 'vue-router'
+import {useSwapStore} from '../../stores/swap'
+import {storeToRefs} from 'pinia'
 import AccountValidationProcess from '../../components/AccountValidationProcess.vue'
-import { onUnmounted } from 'vue'
-import ShowFeeBuy from './components/ShowFeeBuy.vue'
-import ShowFeeSell from './components/ShowFeeSell.vue'
+import {onUnmounted} from 'vue'
+import ShowFeeBuy from "./components/ShowFeeBuy.vue";
+import ShowFeeSell from "./components/ShowFeeSell.vue";
 
 const {
   assetIcon,
@@ -110,11 +110,11 @@ const {
   transactionType,
   assetCode,
   shouldRefreshQuote,
-  feeNoba,
+  feeNoba
 } = storeToRefs(useSwapStore())
-const { t } = useI18n({ useScope: 'global' })
+const {t} = useI18n({useScope: 'global'})
 const router = useRouter()
-const { createQuote, swapHandler, switchTransactionType, clearSwap } = useSwapStore()
+const {createQuote, swapHandler, switchTransactionType, clearSwap} = useSwapStore()
 
 const useUser = useUserStore()
 
