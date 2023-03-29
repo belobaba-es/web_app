a42c66c93ea1dc6be21bdcd3d400b49db168d211
moises serrano <moycs777@gmail.com>
2023-03-29 16:04:43 -0300

    [WIP] Adding some styles and translation keys.

--- src/views/withdraw/components/WitdrawFiat/StepConfirmation.vue
+++ src/views/withdraw/components/WitdrawFiat/StepConfirmation.vue
@@ -55,56 +55,65 @@
       <span class="text-primary">Your transfer has been successful</span>
     </p>
 
+    <!--      transaction summary-->
     <div class="col-12">
-<!--      transaction summary-->
       <span class="mt-4">transaction summary</span>
     <Divider></Divider>
     </div>
 
-    <div class="col-6">
-      <p class="font-medium text-sm">Bank account holder</p>
-      <p class="font-medium text-sm">Email</p>
-      <p class="font-medium text-sm">Wallet address</p>
+    <div class="col-12">
+      <div class="col-6">
+        <p class="font-medium text-sm">{{ t('bankAccountHolder') }}</p>
+        <p class="font-medium text-sm">{{ t('emailLabel') }}</p>
+        <p class="font-medium text-sm">{{ t('walletAddress') }}</p>
+      </div>
+
+      <div class="col-6">
+        <p>Alberto Rodriguez</p>
+        <p>albertorodriguez@gmail.com</p>
+        <p>asdfjasdkfyads^*3HD893459&HD*$</p>
+      </div>
+
+      <Divider type="dashed"></Divider>
+
+      <div class="col-6">
+        <p class="font-medium text-sm">{{ t('emailLabel') }}</p>
+        <p class="font-medium text-sm">{{ t('ourFee') }}</p>
+        <p class="font-medium text-sm">{{ props.formData.realName }} {{ t('hasReceived') }}</p>
+      </div>
+
+      <div class="col-6">
+        <p>{{ props.formData.amount }}</p>
+        <p>{{ props.formData.amountFee }} USD</p>
+        <p>{{ t('Amount') }}</p>
+      </div>
+
+      <Divider type="dashed"></Divider>
+
+      <div class="col-6">
+        <p class="font-medium text-sm">{{ t('transactionNumber') }}</p>
+      </div>
+
+      <div class="col-6">
+        <p>{{ transactionId }}</p>
+      </div>
     </div>
 
-    <div class="col-6">
-      <p>Alberto Rodriguez</p>
-      <p>albertorodriguez@gmail.com</p>
-      <p>asdfjasdkfyads^*3HD893459&HD*$</p>
-    </div>
-
-    <Divider type="dashed"></Divider>
-
-    <div class="col-6">
-      <p class="font-medium text-sm">Enviaste</p>
-      <p class="font-medium text-sm">Email</p>
-      <p class="font-medium text-sm">Wallet address</p>
-    </div>
-
-    <div class="col-6">
-      <p>{{ t('Amount') }}</p>
-      <p>Nuestra comision</p>
-      <p>{{ formData.amountFee }} USD</p>
-    </div>
-
-    <Divider type="dashed"></Divider>
-
-    <div class="col-6">
-      <p class="font-medium text-sm">Numero de Transacion</p>
-    </div>
-
-    <div class="col-6">
-      <p>adf</p>
-    </div>
-
-
-    <Button
-        class="w-50 p-button mt-10"
-        iconPos="right"
-        :label="t('downloadTransferReceipPDF')"
+    <div class="col-12 btn-container">
+      <Button
+        class="w-50 p-button mt-5 btn-routing"
+        :label="t('newTransfer')"
         @click="makeTransaction()"
-        :loading="submitting"
-    />
+      />
+
+      <Button
+        class="w-50 p-button mt-5"
+        :label="t('downloadPdf')"
+        @click="makeTransaction()"
+        :loading="isGeneratingTransactionPDF"
+      />
+    </div>
+
   </div>
 </template>
 
@@ -122,6 +131,8 @@ const toast = useToast()
 const {updateBlockedBalanceWalletByCode} = useBalanceWallet()
 const submitting = ref(false);
 const isCompleted = ref(false);
+const isGeneratingTransactionPDF = ref(false);
+const transactionId = ref(null);
 const {t} = useI18n({useScope: 'global'})
 const route = useRoute();
 const props = defineProps<{
@@ -137,14 +148,15 @@ function makeTransaction() {
   submitting.value = true
 
   console.log("isCompleted", isCompleted.value)
+  console.log("isCompleted", props.formData)
   // return;
 
   withdraw.makeFiatExternalTransfer({
     amount: props.formData.amount,
     beneficiaryId: props.formData.beneficiary.id,
     reference: props.formData.reference,
-  }).then((res) => {
-    console.log('confirmation res', res)
+  }).then((res: any) => {
+    console.log('confirmation res', res.data.transactionId)
     submitting.value = false
     // updateBlockedBalanceWalletByCode('USD', props.formData.amount)
     //
@@ -160,8 +172,6 @@ function makeTransaction() {
     })
 
   })
-
-
 }
 </script>
 
@@ -175,7 +185,24 @@ function makeTransaction() {
   border-color: var(--primary-color);
 }
 
+.mt-5 {
+  margin-top: 22px!important;
+}
+
 .mt-10 {
   margin-top: 42px;
+}
+
+.btn-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.btn-routing {
+  background-color: white;
+  color: black;
+  border: 1px solid #E7E6E7;
+  border-radius: 5px;
+  opacity: 1;
 }
 </style>
