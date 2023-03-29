3de2b5101a05df3d1aa0a6f2bc98ec365eb85ce6
moises serrano <moycs777@gmail.com>
2023-03-29 13:34:12 -0300

    [WIP] Setting basic structure for transaction completed resume.

--- src/views/withdraw/components/WitdrawFiat/StepConfirmation.vue
+++ src/views/withdraw/components/WitdrawFiat/StepConfirmation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="formgrid grid mt-5 mb-5">
+  <div v-if="!isCompleted" class="formgrid grid mt-5 mb-5">
     <div class="col-12">
       <span class="mt-4">{{ t('Confirm wire information') }}</span>
       <Divider></Divider>
@@ -13,7 +13,7 @@
     <div class="col-12 field p-fluid">
 
       <div class="field col-12">
-        <label for="name1">{{ t('Amount') }}</label>
+        <label for="name1">1234 {{ t('Amount') }}</label>
         <p class="green-color">{{ formData.amountFee }} USD</p>
       </div>
 
@@ -48,6 +48,54 @@
         :loading="submitting"
     />
   </div>
+
+  <!-- Completed transfer resume and download PDF Receipt -->
+  <div v-if="isCompleted" class="formgrid grid mt-5 mb-5">
+    <div class="col-12">
+<!--      <span class="mt-4">asdf {{ t('Confirm wire information') }}</span>-->
+      <Divider></Divider>
+    </div>
+    <div>
+<!--      <p class="title-beneficiary">zxcv{{ formData?.beneficiary?.realName }}</p>-->
+<!--      <p class="font-ligth text-base">{{ formData?.beneficiary?.accountNumber }}</p>-->
+    </div>
+    <Divider></Divider>
+
+    <div class="col-12 field p-fluid">
+
+      <div class="field col-12">
+        <label for="name1">1234 {{ t('Amount') }}</label>
+        <p class="green-color">{{ formData.amountFee }} USD</p>
+      </div>
+
+      <div class="field col-12">
+        <small>{{ t('fee') }}</small>
+        <p class="green-color mt-0">
+          <small>{{ formData.fee }} USD</small>
+        </p>
+      </div>
+    </div>
+
+
+    <div class="col-12 mb-2">
+      <p class="text-base">Your are sending to {{ formData?.beneficiary?.realName }}</p>
+
+    </div>
+
+    <div class="col-12 mb-2">
+      <p class="font-medium green-color">
+        {{ formData.amount }} USD
+      </p>
+    </div>
+
+    <Button
+        class="w-50 p-button search-btn"
+        iconPos="right"
+        :label="t('downloadTransferReceipPDF')"
+        @click="makeTransaction()"
+        :loading="submitting"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -63,6 +111,7 @@ import {useToast} from "primevue/usetoast";
 const toast = useToast()
 const {updateBlockedBalanceWalletByCode} = useBalanceWallet()
 const submitting = ref(false);
+const isCompleted = ref(false);
 const {t} = useI18n({useScope: 'global'})
 const route = useRoute();
 const props = defineProps<{
@@ -72,18 +121,23 @@ const props = defineProps<{
 const emit = defineEmits(['complete']);
 
 function makeTransaction() {
+  isCompleted.value = true;
   const withdraw = WithdrawService.instance()
 
   submitting.value = true
+
+  console.log("isCompleted", isCompleted.value)
+  return;
 
   withdraw.makeFiatExternalTransfer({
     amount: props.formData.amount,
     beneficiaryId: props.formData.beneficiary.id,
     reference: props.formData.reference,
   }).then(() => {
+    console.log('confirmation')
     submitting.value = false
-    updateBlockedBalanceWalletByCode('USD', props.formData.amount)
-
+    // updateBlockedBalanceWalletByCode('USD', props.formData.amount)
+    //
     emit('complete')
   }).catch(e => {
     submitting.value = false
