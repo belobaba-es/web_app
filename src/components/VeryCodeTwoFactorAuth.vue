03d548d856bb63ff565c2f1834c1f0ac186410d9
abejarano <programador.angel@gmail.com>
2023-05-03 13:34:27 -0300

    feat: solicitud de recuperacion de doble factor de autenticacion.

--- src/components/VeryCodeTwoFactorAuth.vue
+++ src/components/VeryCodeTwoFactorAuth.vue
@@ -23,12 +23,24 @@
           />
         </div>
 
-        <div class="float-right w-25" v-if="recoveryLink">
+        <div class="float-right w-30" v-if="recoveryLink">
           <p class="green-color cursor-pointer" style="padding-top: 2rem !important" @click="recoveryTwoFactorAuth">
             {{ t('twoFactorAuthRecoveryText') }}
           </p>
         </div>
       </div>
+
+        <div class="w-50 mt-5" style="margin: 0 auto">
+            <Button
+                    type="button"
+                    icon="pi pi-angle-left"
+                    :label="t('backButtonTitle')"
+                    class="font-light mt-lg-5 with-buttons p-button-outlined border-300 sm: mt-5 w-100"
+                    @click="redirectToLogin()"
+            />
+        </div>
+
+
     </div>
   </div>
 </template>
@@ -38,6 +50,8 @@ import InputNumber from 'primevue/inputnumber'
 import Button from 'primevue/button'
 import { useTwoFactorAuth } from '../composables/useTwoFactorAuth'
 import { useI18n } from 'vue-i18n'
+import {useRouter} from "vue-router";
+
 
 interface Props {
   accountId?: string
@@ -46,6 +60,7 @@ interface Props {
 
 const props = defineProps<Props>()
 
+const router = useRouter()
 const { verifyCode, codeForVerify, submitting } = useTwoFactorAuth()
 
 const { t } = useI18n({ useScope: 'global' })
@@ -60,5 +75,9 @@ const makeVerifyCode = async () => {
 
 const recoveryTwoFactorAuth = () => {
   codeIsValidEmit('recoveryTwoFactorAuth', true)
+}
+
+const redirectToLogin = () => {
+    window.location.href = '/'
 }
 </script>
