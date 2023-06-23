77469b12d12f9dff57ccc774ac631ab431d2d79f
moises serrano <moycs777@gmail.com>
2023-06-22 22:53:05 -0300

    [FEAT] Component for transactions into dashboard

--- src/views/dashboard/Index.vue
+++ src/views/dashboard/Index.vue
@@ -3,6 +3,8 @@
     <CardWallet :carousel="true" v-show="useUser.isAccountActive()" />
 
     <AccountValidationProcess v-show="!useUser.isAccountActive()" />
+
+    <TransactionHistoryTable :isDashboard="true"></TransactionHistoryTable>
   </section>
 </template>
 
@@ -10,6 +12,7 @@
 import CardWallet from '../../components/CardWallet.vue'
 import { useUserStore } from '../../stores/user'
 import AccountValidationProcess from '../../components/AccountValidationProcess.vue'
+import TransactionHistoryTable from '../../components/TransactionHistoryTable.vue'
 
 const useUser = useUserStore()
 </script>
