9a169ea956043a4258547f2abe046671fd2117e5
Gregory <Gregory.Arturo.Ruiz@gmail.com>
2023-09-16 17:33:52 -0400

    add input for form setp account

--- src/views/withdraw/components/NewBeneficiaryInternational/StepAccountNewBeneficiaryInternational.vue
+++ src/views/withdraw/components/NewBeneficiaryInternational/StepAccountNewBeneficiaryInternational.vue
@@ -1,38 +1,30 @@
 <template>
   <div class="col-12 md:col-8 mt-5">
     <div class="field">
-      <label>{{ t('depositNameOnBank') }}</label>
-      <div class="p-inputgroup">
-        <InputText type="text" v-model="form.realName" readonly />
-      </div>
-      <small>Make sure it exactly as it appears on your bank account</small>
-    </div>
-
-    <div class="field">
       <label>{{ t('bankName') }}</label>
       <div class="p-inputgroup">
-        <InputText type="text" v-model="form.bankName" />
+        <InputText type="text" v-model="bankName" />
       </div>
     </div>
 
     <div class="field">
       <label>{{ t('accountNumber') }}</label>
       <div class="p-inputgroup">
-        <InputText type="text" v-model="form.accountNumber" />
+        <InputText type="text" v-model="accountNumber" />
       </div>
     </div>
 
     <div class="field">
       <label>{{ t('swiftCode') }}</label>
       <div class="p-inputgroup">
-        <InputText type="text" v-model="form.swiftCode" />
+        <InputText type="text" v-model="swiftCode" />
       </div>
     </div>
 
     <div class="field">
       <label>{{ t('IBAN') }}</label>
       <div class="p-inputgroup">
-        <InputText type="text" v-model="form.iban" />
+        <InputText type="text" v-model="iban" />
       </div>
     </div>
 
@@ -43,136 +35,41 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import { useWorld } from '../../../../composables/useWorld'
+import { ref } from 'vue'
 import { useI18n } from 'vue-i18n'
+
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
-import { BeneficiaryFiatInternacional } from '../../types/beneficiary.interface'
-import router from '../../../../router'
-import { BeneficiaryService } from '../../services/beneficiary'
-import { useToast } from 'primevue/usetoast'
-import showMessage from '../../../../shared/showMessageArray'
-import showExceptionError from '../../../../shared/showExceptionError'
+
 import { useUserStore } from '../../../../stores/user'
 
 const emit = defineEmits(['nextPage', 'prevPage'])
 
 const { t } = useI18n({ useScope: 'global' })
-const toast = useToast()
+
 const submitting = ref(false)
 
 const { getUserName } = useUserStore()
-const {
-  countries,
-  fetchCountries,
-  loadingCountiesField,
-  countriesInputIsEmpty,
-  statesInputIsEmpty,
-  loadingStatesField,
-  states,
-  onChangeCountryHandler,
-  onChangeStateHandler,
-} = useWorld()
 
-const {
-  countries: bankCountries,
-  statesInputIsEmpty: bankStatesInputIsEmpty,
-  loadingStatesField: bankLoadingStatesField,
-  states: bankStates,
-  onChangeCountryHandler: onBankChangeCountryHandler,
-  onChangeStateHandler: onBankChangeStateHandler,
-} = useWorld()
-
-const form = ref<BeneficiaryFiatInternacional>({
-  realName: getUserName(),
-  accountNumber: '',
-  routingNumber: '',
-  streetOne: '',
-  streetTwo: '',
-  postalCode: '',
-  country: '',
-  city: '',
-  state: '',
-  iban: '',
-  bankName: '',
-  swiftCode: '',
-  intermediaryBank: {
-    bankCity: '',
-    intermediaryBankCountry: '',
-    intermediaryBankCity: '',
-    bankPostalCode: '',
-    intermediaryBankPostalCode: '',
-    intermediaryBankState: '',
-    intermediaryBankName: '',
-    intermediaryBankStreetTwo: '',
-    intermediaryBankAccountNumber: '',
-    intermediaryBankStreetOne: '',
-    bankCountry: '',
-    intermediaryNumber: '',
-    bankState: '',
-    bankStreetTwo: '',
-    bankStreetOne: '',
-    intermediaryNumberType: 'ROUTING_NUMBER',
-  },
-  typeBeneficiaryBankWithdrawal: 'INTERNATIONAL',
-})
-
-onMounted(() => {
-  fetchCountries(true).then(() => {
-    bankCountries.value = countries.value
-  })
-})
-
-const back = () => {
-  router.push('/withdraw/fiat/international')
-}
-
-const saveBeneficiary = () => {
-  submitting.value = true
-  const beneficiaryService = BeneficiaryService.instance()
-  beneficiaryService
-    .saveBeneficiaryInternational(form.value)
-    .then(resp => {
-      submitting.value = false
-      router.push('/withdraw/fiat/international')
-      toast.add({
-        severity: 'success',
-        detail: resp.message,
-        life: 4000,
-      })
-    })
-    .catch(e => {
-      submitting.value = false
-
-      if (e.response.data.data?.warning) {
-        e.response.data.data.warning.forEach((element: any) => {
-          showExceptionError(toast, 'error', t('somethingWentWrong'), `${element.field} ${element.message}`, 4000)
-        })
-        return
-      }
-
-      if (e.response.data.message) {
-        showExceptionError(toast, 'error', t('somethingWentWrong'), e.response.data.message, 4000)
-        return
-      }
-
-      showMessage(toast, e.response.data)
-    })
-}
+const bankName = ref<string>('')
+const accountNumber = ref<string>('')
+const swiftCode = ref<string>('')
+const iban = ref<string>('')
+const formData = ref<object>()
 
 const nextStep = () => {
   const page = 0
 
-  const formData = {
-    accountNumber: form.value.accountNumber,
-  }
-
-  // console.log(formData)
-
+  formData.value = {
+    bankName: bankName,
+    accountNumber: accountNumber,
+    swiftCode: swiftCode,
+    iban: iban,
+  } 
+  
   emit('nextPage', {
     pageIndex: page,
-    formData: formData,
+    formData: formData.value,
   })
 }
 </script>
