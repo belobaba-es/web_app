e8fe5d917e7ec0aa31491372b53d0dc1563a1f10
Gregory <Gregory.Arturo.Ruiz@gmail.com>
2023-09-28 07:22:58 -0400

    finish page

--- src/views/register/CreateUser.vue
+++ src/views/register/CreateUser.vue
@@ -56,100 +56,62 @@
                 </span>
               </label>
             </div>
-
-            <!-- <div class="text-right">
-              <RouterLink to="/forgot-password">
-                {{ t('recoveryPassword') }}
-              </RouterLink>
-            </div> -->
           </div>
 
           <div class="flex justify-content-between align-items-center">
             <div class="field-checkbox mt-2">
               <Checkbox inputId="patriot" v-model="form.patriot" :binary="true" />
-              <label for="patriot"> I accept the Patriot Act </label>
+              <label for="patriot"> {{t('iAcceptPatriot')}} <a href="#" @click="visible = true">{{ t('patrioticLaw') }}</a></label>
             </div>
           </div>
 
-          <Button label="Show" icon="pi pi-external-link" @click="visible = true" />
+          <Dialog
+            v-model:visible="visible"
+            modal
+            :style="{ width: '40vw' }"
+            :breakpoints="{ '1560px': '75vh', '960px': '75vw', '641px': '90vw' }"
+          >
+            <template style="background-color: black" #header>
+              <p class="text-4xl font-medium">Patriot Act</p>
+            </template>
 
-          <Dialog v-model:visible="visible" modal header="Header" :style="{ width: '50vw' }">
-            <p class="mb-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-              mollit anim id est laborum.
-            </p>
-            <p class="mb-5">
-              "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam
-              rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-              explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-              consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-              dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora
-              incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-              exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
-              vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui
-              dolorem eum fugiat quo voluptas nulla pariatur?
-            </p>
-            <p class="mb-5">
-              At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti
-              atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique
-              sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum
-              facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
-              impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor
-              repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut
-              et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente
-              delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores
-              repellat.
-            </p>
-            <p class="mb-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-              mollit anim id est laborum.
-            </p>
-            <p class="mb-5">
-              "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam
-              rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-              explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-              consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-              dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora
-              incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-              exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
-              vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui
-              dolorem eum fugiat quo voluptas nulla pariatur?
-            </p>
-            <p class="mb-5">
-              At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti
-              atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique
-              sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum
-              facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
-              impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor
-              repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut
-              et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente
-              delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores
-              repellat.
-            </p>
+            <usPatriot />
+
+            <template #footer>
+              <div class="flex justify-content-between mt-5">
+                <Button
+                  :label="t('deny')"
+                  icon="pi pi-times"
+                  @click="
+                    () => {
+                      form.patriot = false
+                      visible = false
+                    }
+                  "                  text
+                  class="font-light p-button-outlined border-300"
+                />
+                <Button
+                  :label="t('accept')"
+                  icon="pi pi-check"
+                  @click="
+                    () => {
+                      form.patriot = true
+                      visible = false
+                    }
+                  "
+                  autofocus
+                />
+              </div>
+            </template>
           </Dialog>
 
-          <div class="container-flex mt-lg-2">
-            <div class="float-left w-25">
-              <Button
-                type="button"
-                icon="pi pi-angle-left"
-                :label="t('backButtonTitle')"
-                class="font-light w-100 border-300 p-button-outlined"
-              />
-            </div>
-            <div class="float-right w-25">
+          <div class="container-flex">
+            <div class="mt-3 w-100">
               <Button
                 type="submit"
                 icon="pi pi-angle-right"
-                iconPos="right"
-                label="Login"
-                class="font-light w-100"
+                :label="t('save')"
+                class="font-light with-buttons"
                 :loading="submitting"
               />
             </div>
@@ -157,8 +119,8 @@
 
           <Button
             type="button"
-            :label="t('noAccount')"
-            class="font-light mt-lg-5 with-buttons p-button-outlined border-300 sm: mt-5"
+            :label="t('alreadyAccount')"
+            class="font-light mt-lg-3 with-buttons p-button-outlined border-300"
             @click="redirectLogin()"
           />
         </form>
@@ -187,8 +149,11 @@ import { useRouter } from 'vue-router'
 import { useToast } from 'primevue/usetoast'
 import Checkbox from 'primevue/checkbox'
 
+//import a usPatriot component
+import usPatriot from './components/usPatriotEs.vue'
+
 const submitting = ref(false)
-const visible = ref(false);
+const visible = ref(false)
 const toast = useToast()
 const { t } = useI18n({ useScope: 'global' })
 
@@ -291,5 +256,8 @@ const handleSubmit = () => {
 .logo-noba {
   width: 142px;
   height: 64px;
+}
+.p-dialog .p-dialog-header {
+  background-color: black !important;
 }
 </style>
