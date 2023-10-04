7083a63b17c3af214e291d6e4be27f35a2ff0f8a
Moises Serrano <moycs777@gmail.com>
2023-10-04 15:11:36 -0300

    [FIX] Removing duplicated finish register in dashboard

--- src/components/TransactionHistoryTable.vue
+++ src/components/TransactionHistoryTable.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="section-main">
-    <FinishRegisterWarningBar></FinishRegisterWarningBar>
+    <FinishRegisterWarningBar v-if="!props.isDashboard"></FinishRegisterWarningBar>
 
     <p class="text-3xl font-medium">{{ t('transactionHistory') }}</p>
 
