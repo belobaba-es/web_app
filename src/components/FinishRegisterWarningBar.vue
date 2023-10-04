f84a0e23e87f5cb3727201b072b1f9ad1f8dbd93
Moises Serrano <moycs777@gmail.com>
2023-10-04 15:23:19 -0300

    [FIX] Removing duplicated finish register in dashboard

--- src/components/FinishRegisterWarningBar.vue
+++ src/components/FinishRegisterWarningBar.vue
@@ -1,25 +1,29 @@
-<template v-show="!areDocumentsUploaded()">
-  <router-link to="/upload-documents">
+<template v-show="isVisible">
+  <router-link v-show="isVisible" to="/upload-documents">
     <Message severity="success" :closable="false">{{ t('uploadDocuments') }}!</Message>
   </router-link>
 </template>
 
 <script setup lang="ts">
-import Message from 'primevue/message'
-
 import { useI18n } from 'vue-i18n'
 import { AccountStatus } from '../views/login/types/login.interface'
 import { useUserStore } from '../stores/user'
+import { onMounted, ref } from 'vue'
 
 const { t } = useI18n({ useScope: 'global' })
 const useUser = useUserStore()
+const isVisible = ref(false)
+
+onMounted(() => {
+  areDocumentsUploaded()
+})
 
 const areDocumentsUploaded = () => {
   if (useUser.getUser.client == undefined || useUser.getUser.client.status == undefined) {
-    return false
+    return
   }
 
-  return useUser.getUser.client.status === AccountStatus.APPROVED
+  isVisible.value = useUser.getUser.client.status == AccountStatus.REGISTERED
 }
 </script>
 
