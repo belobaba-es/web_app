ac44e342e7be2ef90450f59b3deab1b1a9a8f73e
Jonathan Jimenez <programador.jimenez@gmail.com>
2023-10-05 22:47:16 -0300

    Fix - Modificaciones visual

--- src/views/upload-documents/components/personal/Step2.vue
+++ src/views/upload-documents/components/personal/Step2.vue
@@ -20,51 +20,57 @@
                 <div class="px-3 pt-3 pb-0">
                   <div class="field">
                     <div class="grid">
-                      <div class="col-8">
+                      <div class="col-12">
                         <div class="field col-12">
-                          <div class="col-12">
-                            <div class="field">
-                              <label>{{ t('Document Type') }}</label>
-                              <div class="p-inputgroup">
-                                <Dropdown
-                                    :options="data"
-                                    optionLabel="label"
-                                    option-value="value"
-                                    class="w-full"
-                                />
+                          
+                          <div class="field">
+                            <label>{{ t('Document Type') }}</label>
+                            <div class="p-inputgroup">
+                              <Dropdown :options="data" optionLabel="label" option-value="value" class="w-full" />
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="px-3 pb-0">
+                          <div class="field">
+                            <div class="grid">
+                              <div class="col-6">
+                                <label>{{ t('Front side') }}</label>
+                                <FileInput label="other" side="front" type="other" :account-id="accountId ?? ''"
+                                  :document-country="getOwner()?.taxCountry ?? 'US'" :tax-id="getOwner()?.taxId ?? ''"
+                                  :is-company="true" />
+                              </div>
+
+                              <div class="col-6">
+                                <label>{{ t('Back side') }}</label>
+                                <FileInput label="other" side="front" type="other" :account-id="accountId ?? ''"
+                                  :document-country="getOwner()?.taxCountry ?? 'US'" :tax-id="getOwner()?.taxId ?? ''"
+                                  :is-company="true" />
                               </div>
                             </div>
                           </div>
                         </div>
-                        <div
-                          class="flex justify-content-between align-items-center p-3 mt-5 border-round bg-primary cursor-pointer">
-                          <span>Seleccionar archivo</span>
-                          <span>
-                            <i class="pi pi-upload"></i><input id="frontyes" type="file">
-                          </span>
+
+                        <div class="px-3 py-3">
+                          <div class="field">
+                            <div class="grid">
+                              <div class="col-6">
+                                <label>{{ t('utilityBillLabel') }}</label>
+                                <div class="mt-2 mb-4">
+                                  <Dropdown v-model="proofOfAddress" :options="documentTypeProofOfAddress"
+                                    option-label="name" option-value="value" :placeholder="t('documentTypePlaceHolder')"
+                                    class="w-full" @change="selectedProofOfAddress" />
+                                </div>
+                                <FileInput :label="getSelectedTypeDocumentProofOfAddress('0')" side="front"
+                                  :type="getSelectedTypeDocumentProofOfAddress('0')" :account-id="accountId ?? ''"
+                                  :document-country="getOwner()?.taxCountry ?? 'US'" :tax-id="getOwner()?.taxId ?? ''"
+                                  :is-company="true" />
+                              </div>
+                            </div>
+                          </div>
                         </div>
                       </div>
 
-                    </div>
-                  </div>
-                </div>
-
-                <div class="p-divider p-component p-divider-horizontal p-divider-solid p-divider-left" role="separator"
-                  aria-orientation="horizontal"></div>
-
-                <div class="px-3 py-3">
-                  <div class="field">
-                    <div class="grid">
-                      <div class="col-8">
-                        <label>Documentos constitutivos de la empresa</label>
-                        <div class="mt-2 mb-4"></div>
-
-                        <div
-                          class="flex justify-content-between align-items-center p-3 border-round bg-primary cursor-pointer">
-                          <span>Seleccionar archivo</span>
-                          <span><i class="pi pi-upload"></i><input id="otherfrontyes" type="file"></span>
-                        </div>
-                      </div>
                     </div>
                   </div>
                 </div>
@@ -77,13 +83,38 @@
   </section>
 </template>
 
-<script lang="ts" setup>
-import { ref, onMounted, onBeforeMount } from 'vue'
-import { useI18n } from 'vue-i18n'
+<script setup lang="ts">
 import Dropdown from 'primevue/dropdown'
-import { Document } from '../../../profile/types/account.interface';
+import { ref, onBeforeMount } from 'vue'
+import Divider from 'primevue/divider'
+import { useAccount } from '../../../../composables/useAccount'
+import { useI18n } from 'vue-i18n'
+import FileInput from '../../../profile/components/FileInput.vue'
+import FileUpload from './FileUploaded.vue'
+import { useDocuments } from '../../../../composables/useDocuments'
 
 const { t } = useI18n({ useScope: 'global' })
+const { getOwner, accountId } = useAccount()
+const { addDocument, setSelectedTypeDocumentProofOfAddress, getSelectedTypeDocumentProofOfAddress } = useDocuments()
+
+const documentTypeProofOfAddress = ref([
+  { value: 'monthly_utility', name: t('documentProofOfAddress1') },
+  { value: 'statements', name: t('documentProofOfAddress2') },
+  { value: 'rental_lease_agreement', name: t('documentProofOfAddress3') },
+  { value: 'vehicle_registration', name: t('documentProofOfAddress4') },
+  { value: 'real_estate_property_title', name: t('documentProofOfAddress5') },
+  { value: 'property_tax_bill', name: t('documentProofOfAddress6') },
+  { value: 'w2', name: t('documentProofOfAddress7') },
+])
+
+const proofOfAddress = ref('')
+
+onBeforeMount(() => {
+  addDocument('0', {
+    selectedTypeDocumentProofOfAddress: '',
+    selectedTypeIdentificationDocument: '',
+  })
+})
 
 const data = [
   { id: 1, name: 'Ram' },
@@ -94,7 +125,10 @@ const data = [
   { id: 6, name: 'Arjun' },
 ]
 
-const selectedItem = ref({})
-
+const selectedProofOfAddress = (e: any) => {
+  setSelectedTypeDocumentProofOfAddress('0', e.value)
+}
 </script>
+
+<style scoped></style>
 
