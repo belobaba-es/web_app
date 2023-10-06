54fe70fceb42e6c67fa986043b7963863439d7fa
Gregory <Gregory.Arturo.Ruiz@gmail.com>
2023-10-06 01:41:26 -0400

    add btn create sharedholder

--- src/views/upload-documents/components/business/Step2.vue
+++ src/views/upload-documents/components/business/Step2.vue
@@ -7,7 +7,38 @@
       <p class="text-3xl font-medium">{{ t('titleBusinessAccount') }}</p>
     </div>
 
-    <div class="formgrid grid col-12 sm:col-12 md:col-12 lg:col-8 xl:col-8"></div>
+    <div class="formgrid grid col-12 sm:col-12 md:col-12 lg:col-8 xl:col-8">
+        <!-- <div v-for="(member, idx) in members" class="col-6 sm:col-6 md:col-6 lg:col-4 xl:col-3" :key="idx">
+          <div
+            class="p-3 border-1 border-gray-300 border-round-2xl flex-column cursor-pointer"
+            @click="showDetails(member)"
+          >
+            <div class="mb-2">
+              <img src="../../assets/icons/icon-user.svg" />
+            </div>
+            <div>
+              <p class="text-lg">{{ getFullName(member) }}</p>
+            </div>
+            <div class="flex justify-content-between align-items-center">
+              <span class="text-base">{{ member.label }}</span>
+              <Button label="Remove" class="p-button-text" icon="pi pi-trash" icon-pos="right" />
+            </div>
+          </div>
+        </div> -->
+        <div class="col-5 sm:col-5 md:col-5 lg:col-3 xl:col-3">
+          <div
+            class="p-3 border-1 border-cyan-500 border-round-2xl flex-column bg-gray-100 cursor-pointer"
+            @click="onCreateNewShareholder"
+          >
+            <div>
+              <img src="../../../../assets/icons/user-add.svg" />
+            </div>
+            <div>
+              <p class="text-lg">{{ t('addNewShareHolder') }}</p>
+            </div>
+          </div>
+        </div>
+    </div>
   </section>
 </template>
 
@@ -18,9 +49,7 @@ import { useToast } from 'primevue/usetoast'
 
 import { useI18n } from 'vue-i18n'
 
-
 import router from '../../../../router'
-
 
 const { t } = useI18n({ useScope: 'global' })
 
@@ -28,12 +57,12 @@ const toast = useToast()
 
 const submitting = ref(false)
 
+onMounted(async () => {})
 
 
-onMounted(async () => {
-})
-
-
+const onCreateNewShareholder = () => {
+  router.push('/upload-documents/business/step1')
+}
 
 const saveData = () => {
   submitting.value = true
@@ -41,9 +70,7 @@ const saveData = () => {
   if (true) {
     const formData = ref()
 
-    formData.value = {
-      
-    }
+    formData.value = {}
 
     //set data to localstorage
     localStorage.setItem('companyData', JSON.stringify(formData.value))
