054798c1d51d4752f4c33744a0c38a0c1f65da90
Gregory <Gregory.Arturo.Ruiz@gmail.com>
2023-10-12 20:13:24 -0400

    fix states

--- src/views/withdraw/components/NewBeneficiary/StepBankNewBeneficiary.vue
+++ src/views/withdraw/components/NewBeneficiary/StepBankNewBeneficiary.vue
@@ -46,17 +46,7 @@
       <div class="field col-4">
         <label>{{ t('stateLabel') }}</label>
         <div class="p-inputgroup">
-          <Dropdown
-            v-model="bankState"
-            :options="states"
-            optionLabel="name"
-            option-value="state_code"
-            :loading="loadingStatesField"
-            :placeholder="t('statePlaceHolder')"
-            :disabled="statesInputIsEmpty"
-            class="w-full"
-            @change="onChangeStateHandler"
-          />
+          <InputText type="text" v-model="bankState" />
         </div>
       </div>
 
