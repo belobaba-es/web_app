8b2b84846d5d993bce2381e7b334ac4112cbbdf6
Gregory <Gregory.Arturo.Ruiz@gmail.com>
2023-10-17 05:47:03 -0400

    fix

--- src/views/onboarding/components/business/Step4.vue
+++ src/views/onboarding/components/business/Step4.vue
@@ -18,9 +18,28 @@
               <div class="p-scrollpanel-wrapper">
                 <div class="p-scrollpanel-content">
                   <div class="px-3 pt-3 pb-0">
-                    <div class="field" v-for="(shareholder, idx) in partners" :key="idx">
-                      <DocumentsShareholders :dni="shareholder.dni" :tax-id="shareholder.taxId" />
-                    </div>
+                    <DocumentsCompany :dni="formData.informationCompany.registerNumber" tax-id="formData.informationCompany.registerNumber"/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-12 sm:col-12 md:col-12 lg:col-6 xl:col-6 p-panel p-component shareholders-panel">
+      <div class="p-panel-header">
+        <span class="p-panel-title">{{ t('uploadDocumentsTitle') }}</span>
+      </div>
+      <div class="p-toggleable-content" role="region">
+        <div class="p-panel-content">
+          <div class="p-scrollpanel p-component custom" style="height: 400px">
+            <div class="p-scrollpanel-wrapper">
+              <div class="p-scrollpanel-content">
+                <div class="px-3 pt-3 pb-0">
+                  <div class="field" v-for="(shareholder, idx) in partners" :key="idx">
+                    <DocumentsShareholders :dni="shareholder.dni" :tax-id="shareholder.taxId" />
                   </div>
                 </div>
               </div>
@@ -36,6 +55,7 @@
 import { ref, onBeforeMount, defineProps, onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
 import DocumentsShareholders from '../DocumentsShareholders.vue'
+import DocumentsCompany from '../DocumentsCompany.vue'
 
 const { t } = useI18n({ useScope: 'global' })
 
