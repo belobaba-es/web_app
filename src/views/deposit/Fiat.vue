ce7d2a4f5d4f8b9bf2d12dbe8ae60e52c3f14d51
jlanza1981 <lanza.johanna@gmail.com>
2024-02-06 19:59:22 -0400

    correcion en titulo pdf de instruciones de deposito

--- src/views/deposit/Fiat.vue
+++ src/views/deposit/Fiat.vue
@@ -3,7 +3,7 @@
     <p class="text-3xl font-medium mb-4">
       {{ t('deposit') }} / <span class="text-primary">{{ t('fiat') }} </span>
     </p>
-    <TabMenu :model="menuItems" v-model:activeIndex="active" />
+    <TabMenu :model="menuItems" v-model:activeIndex="active"/>
 
     <div v-if="active == 0" class="mt-2">
       <div class="flex justify-content-start align-items-center">
@@ -16,57 +16,57 @@
         <p class="mb-0">{{ bankNational?.bankName }}</p>
         <i v-if="bankNational?.bankName" @click="copyToClipboard(bankNational?.bankName)" class="pi pi-clone"></i>
       </div>
-      <Divider type="solid" />
+      <Divider type="solid"/>
 
       <p class="font-medium text-sm">ABA Fedwire</p>
       <div class="flex justify-content-between align-items-center">
         <p class="mb-0">{{ bankNational?.routingNumber }}</p>
         <i
-          v-if="bankNational?.routingNumber"
-          @click="copyToClipboard(bankNational?.routingNumber)"
-          class="pi pi-clone"
+            v-if="bankNational?.routingNumber"
+            @click="copyToClipboard(bankNational?.routingNumber)"
+            class="pi pi-clone"
         ></i>
       </div>
-      <Divider type="solid" />
+      <Divider type="solid"/>
 
       <p class="font-medium text-sm">{{ t('creditTo') }}</p>
       <div class="flex justify-content-between align-items-center">
         <p class="mb-0">{{ bankNational?.creditTo }}</p>
         <i v-if="bankNational?.creditTo" @click="copyToClipboard(bankNational?.creditTo)" class="pi pi-clone"></i>
       </div>
-      <Divider type="solid" />
+      <Divider type="solid"/>
 
       <p class="font-medium text-sm">{{ t('reference') }}</p>
       <div class="flex justify-content-between align-items-center">
         <p class="mb-0">{{ bankNational?.reference }}</p>
         <i v-if="bankNational?.reference" @click="copyToClipboard(bankNational?.reference)" class="pi pi-clone"></i>
       </div>
-      <Divider type="solid" />
+      <Divider type="solid"/>
 
       <p class="font-medium text-sm">{{ t('address') }}</p>
       <div class="flex justify-content-between align-items-center">
         <p class="mb-0">{{ bankNational?.address }}</p>
         <i v-if="bankNational?.address" @click="copyToClipboard(bankNational?.address)" class="pi pi-clone"></i>
       </div>
-      <Divider type="solid" />
+      <Divider type="solid"/>
 
       <p class="font-medium text-sm">{{ t('accountNumber') }}</p>
       <div class="flex justify-content-between align-items-center">
         <p class="mb-0">{{ bankNational?.accountNumber }}</p>
         <i
-          v-if="bankNational?.accountNumber"
-          @click="copyToClipboard(bankNational?.accountNumber)"
-          class="pi pi-clone"
+            v-if="bankNational?.accountNumber"
+            @click="copyToClipboard(bankNational?.accountNumber)"
+            class="pi pi-clone"
         ></i>
       </div>
-      <Divider type="solid" />
+      <Divider type="solid"/>
 
       <p class="font-medium text-sm">{{ t('bankAddress') }}</p>
       <div class="flex justify-content-between align-items-center">
         <p class="mb-0">{{ bankNational?.bankAddress }}</p>
         <i v-if="bankNational?.bankAddress" @click="copyToClipboard(bankNational?.bankAddress)" class="pi pi-clone"></i>
       </div>
-      <Divider type="solid" />
+      <Divider type="solid"/>
 
       <p class="font-medium text-sm">{{ t('bankPhone') }}</p>
       <div class="flex justify-content-between align-items-center">
@@ -77,12 +77,12 @@
       <div class="grid mt-2">
         <div class="col-12 sm:col-12 md:col-12 lg:col-6 xl:col-6">
           <Button
-            icon="pi pi-angle-right"
-            iconPos="right"
-            :loading="submitting"
-            class="p-button download-btn"
-            :label="t('downloadPdf')"
-            @click="generatePdfNationalData"
+              icon="pi pi-angle-right"
+              iconPos="right"
+              :loading="submitting"
+              class="p-button download-btn"
+              :label="t('downloadPdf')"
+              @click="generatePdfNationalData"
           />
         </div>
       </div>
@@ -98,109 +98,97 @@
       <div class="flex justify-content-between align-items-center">
         <p class="">{{ bankInternational?.bankName }}</p>
         <i
-          v-if="bankInternational?.bankName"
-          @click="copyToClipboard(bankInternational?.bankName)"
-          class="pi pi-clone"
+            v-if="bankInternational?.bankName"
+            @click="copyToClipboard(bankInternational?.bankName)"
+            class="pi pi-clone"
         ></i>
       </div>
-      <Divider type="solid" />
-
-      <p class="font-medium text-sm">{{ t('routingNumber') }}</p>
-      <div class="flex justify-content-between align-items-center">
-        <p class="">{{ bankInternational?.routingNumber }}</p>
-        <i
-          v-if="bankInternational?.routingNumber"
-          @click="copyToClipboard(bankInternational?.routingNumber)"
-          class="pi pi-clone"
-        ></i>
-      </div>
-      <Divider type="solid" />
-
+      <Divider type="solid"/>
       <p class="font-medium text-sm">{{ t('swiftCode') }}</p>
       <div class="flex justify-content-between align-items-center">
         <p class="">{{ bankInternational?.swiftCode }}</p>
         <i
-          v-if="bankInternational?.swiftCode"
-          @click="copyToClipboard(bankInternational?.swiftCode)"
-          class="pi pi-clone"
+            v-if="bankInternational?.swiftCode"
+            @click="copyToClipboard(bankInternational?.swiftCode)"
+            class="pi pi-clone"
         ></i>
       </div>
-      <Divider type="solid" />
+      <Divider type="solid"/>
 
       <p class="font-medium text-sm">{{ t('creditTo') }}</p>
       <div class="flex justify-content-between align-items-center">
         <p class="">{{ bankInternational?.creditTo }}</p>
         <i
-          v-if="bankInternational?.creditTo"
-          @click="copyToClipboard(bankInternational?.creditTo)"
-          class="pi pi-clone"
+            v-if="bankInternational?.creditTo"
+            @click="copyToClipboard(bankInternational?.creditTo)"
+            class="pi pi-clone"
         ></i>
       </div>
-      <Divider type="solid" />
+      <Divider type="solid"/>
 
       <p class="font-medium text-sm">{{ t('reference') }}</p>
       <div class="flex justify-content-between align-items-center">
         <p class="">{{ bankInternational?.reference }}</p>
         <i
-          v-if="bankInternational?.reference"
-          @click="copyToClipboard(bankInternational?.reference)"
-          class="pi pi-clone"
+            v-if="bankInternational?.reference"
+            @click="copyToClipboard(bankInternational?.reference)"
+            class="pi pi-clone"
         ></i>
       </div>
-      <Divider type="solid" />
+      <Divider type="solid"/>
 
       <p class="font-medium text-sm">{{ t('address') }}</p>
       <div class="flex justify-content-between align-items-center">
         <p class="">{{ bankInternational?.address }}</p>
         <i
-          v-if="bankInternational?.address"
-          @click="copyToClipboard(bankInternational?.address)"
-          class="pi pi-clone"
+            v-if="bankInternational?.address"
+            @click="copyToClipboard(bankInternational?.address)"
+            class="pi pi-clone"
         ></i>
       </div>
-      <Divider type="solid" />
+      <Divider type="solid"/>
 
       <p class="font-medium text-sm">{{ t('accountNumber') }}</p>
       <div class="flex justify-content-between align-items-center">
         <p class="">{{ bankInternational?.accountNumber }}</p>
         <i
-          v-if="bankInternational?.accountNumber"
-          @click="copyToClipboard(bankInternational?.accountNumber)"
-          class="pi pi-clone"
+            v-if="bankInternational?.accountNumber"
+            @click="copyToClipboard(bankInternational?.accountNumber)"
+            class="pi pi-clone"
         ></i>
       </div>
-      <Divider type="solid" />
+      <Divider type="solid"/>
 
       <p class="font-medium text-sm">{{ t('bankAddress') }}</p>
       <div class="flex justify-content-between align-items-center">
         <p class="">{{ bankInternational?.bankAddress }}</p>
         <i
-          v-if="bankInternational?.bankAddress"
-          @click="copyToClipboard(bankInternational?.bankAddress)"
-          class="pi pi-clone"
+            v-if="bankInternational?.bankAddress"
+            @click="copyToClipboard(bankInternational?.bankAddress)"
+            class="pi pi-clone"
         ></i>
       </div>
-      <Divider type="solid" />
+      <Divider type="solid"/>
 
       <p class="font-medium text-sm">{{ t('bankPhone') }}</p>
       <div class="flex justify-content-between align-items-center">
         <p class="">{{ bankInternational?.bankPhone }}</p>
         <i
-          v-if="bankInternational?.bankPhone"
-          @click="copyToClipboard(bankInternational?.bankPhone)"
-          class="pi pi-clone"
+            v-if="bankInternational?.bankPhone"
+            @click="copyToClipboard(bankInternational?.bankPhone)"
+            class="pi pi-clone"
         ></i>
       </div>
 
       <div class="grid mt-2">
         <div class="col-12 sm:col-12 md:col-12 lg:col-4 xl:col-4">
           <Button
-            icon="pi pi-angle-right"
-            iconPos="right"
-            :loading="submitting"
-            class="p-button download-btn"
-            :label="t('downloadPdf')"
-            @click="generatePdfInternationalData"
+              icon="pi pi-angle-right"
+              iconPos="right"
+              :loading="submitting"
+              class="p-button download-btn"
+              :label="t('downloadPdf')"
+              @click="generatePdfInternationalData"
           />
         </div>
       </div>
@@ -209,31 +197,32 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue'
+import {ref} from 'vue'
 import Divider from 'primevue/divider'
 import TabMenu from 'primevue/tabmenu'
 import Button from 'primevue/button'
-import { useI18n } from 'vue-i18n'
-import { FiatService } from './services/fiat'
-import { BankData } from './types/fiat.interface'
+import {useI18n} from 'vue-i18n'
+import {FiatService} from './services/fiat'
+import {BankData} from './types/fiat.interface'
 
 import logo from '../../assets/img/logo.png'
 
 import generatePdf from '../../shared/generatePdf'
-import { useToast } from 'primevue/usetoast'
-import { useAuth } from '../../composables/useAuth'
+import {useToast} from 'primevue/usetoast'
+import {useAuth} from '../../composables/useAuth'
 
 interface tabItem {
   label: string
   icon?: string
   to?: string
 }
+
 const toast = useToast()
 
 const submitting = ref(false)
 
-const { t } = useI18n({ useScope: 'global' })
-const { getUserName } = useAuth()
+const {t} = useI18n({useScope: 'global'})
+const {getUserName} = useAuth()
 
 const username = getUserName()
 
@@ -243,40 +232,41 @@ const bankInternational = ref()
 
 const bankNationalPdf: any = {}
 const bankInternationalPdf: any = {}
-const title = t('titleDespositFiat')
+const title = t('namePdfDepositFiatDomestic')
+const titleInternacional = t('namePdfDepositFiatInternational')
 const footerPdf = t('footerPdfNobaData')
 
 new FiatService()
-  .bankData()
-  .then(data => {
-    submitting.value = false
-    dataBank.value = data
-    bankNational.value = dataBank.value.find(bank => bank.typeBankingData == 'DOMESTIC')
-    if (!bankNational.value) {
-      bankNational.value = dataBank.value.find(bank => bank.typeBankingData == 'NATIONAL')
-    }
+    .bankData()
+    .then(data => {
+      submitting.value = false
+      dataBank.value = data
+      bankNational.value = dataBank.value.find(bank => bank.typeBankingData == 'DOMESTIC')
+      if (!bankNational.value) {
+        bankNational.value = dataBank.value.find(bank => bank.typeBankingData == 'NATIONAL')
+      }
 
-    bankNationalPdf[t('depositBankName') + ':'] = bankNational.value.bankName
-    bankNationalPdf['ABA Fedwire:'] = bankNational.value.routingNumber
-    bankNationalPdf[t('creditTo') + ':'] = bankNational.value.creditTo
-    bankNationalPdf[t('reference') + ':'] = bankNational.value.reference
-    bankNationalPdf[t('address') + ':'] = bankNational.value.address
-    bankNationalPdf[t('accountNumber') + ':'] = bankNational.value.accountNumber
-    bankNationalPdf[t('bankAddress') + ':'] = bankNational.value.bankAddress
-    bankNationalPdf[t('bankPhone') + ':'] = bankNational.value.bankPhone
+      bankNationalPdf[t('depositBankName') + ':'] = bankNational.value.bankName
+      bankNationalPdf['ABA Fedwire:'] = bankNational.value.routingNumber
+      bankNationalPdf[t('creditTo') + ':'] = bankNational.value.creditTo
+      bankNationalPdf[t('reference') + ':'] = bankNational.value.reference
+      bankNationalPdf[t('address') + ':'] = bankNational.value.address
+      bankNationalPdf[t('accountNumber') + ':'] = bankNational.value.accountNumber
+      bankNationalPdf[t('bankAddress') + ':'] = bankNational.value.bankAddress
+      bankNationalPdf[t('bankPhone') + ':'] = bankNational.value.bankPhone
 
-    bankInternational.value = dataBank.value.find(bank => bank.typeBankingData == 'INTERNATIONAL')
+      bankInternational.value = dataBank.value.find(bank => bank.typeBankingData == 'INTERNATIONAL')
 
-    bankInternationalPdf[t('depositBankName') + ':'] = bankInternational.value.bankName
-    bankInternationalPdf[t('swiftCode') + ':'] = bankInternational.value.swiftCode
-    bankInternationalPdf[t('creditTo') + ':'] = bankInternational.value.creditTo
-    bankInternationalPdf[t('reference') + ':'] = bankInternational.value.reference
-    bankInternationalPdf[t('address') + ':'] = bankInternational.value.address
-    bankInternationalPdf[t('accountNumber') + ':'] = bankInternational.value.accountNumber
-    bankInternationalPdf[t('bankAddress') + ':'] = bankInternational.value.bankAddress
-    bankInternationalPdf[t('bankPhone') + ':'] = bankInternational.value.bankPhone
-  })
-  .catch(() => (submitting.value = false))
+      bankInternationalPdf[t('depositBankName') + ':'] = bankInternational.value.bankName
+      bankInternationalPdf[t('swiftCode') + ':'] = bankInternational.value.swiftCode
+      bankInternationalPdf[t('creditTo') + ':'] = bankInternational.value.creditTo
+      bankInternationalPdf[t('reference') + ':'] = bankInternational.value.reference
+      bankInternationalPdf[t('address') + ':'] = bankInternational.value.address
+      bankInternationalPdf[t('accountNumber') + ':'] = bankInternational.value.accountNumber
+      bankInternationalPdf[t('bankAddress') + ':'] = bankInternational.value.bankAddress
+      bankInternationalPdf[t('bankPhone') + ':'] = bankInternational.value.bankPhone
+    })
+    .catch(() => (submitting.value = false))
 
 const active = ref<number>(0)
 
@@ -291,12 +281,14 @@ const menuItems = ref<tabItem[]>([
 
 const generatePdfNationalData = () => {
   const nameFile = `${username} ${t('namePdfDepositFiatDomestic')}`
+  console.log('bankInternationalPdf', bankInternationalPdf)
   generatePdf(nameFile, logo, title, bankNationalPdf, footerPdf)
 }
 
 const generatePdfInternationalData = () => {
   const nameFile = `${username} ${t('namePdfDepositFiatInternational')}`
-  generatePdf(nameFile, logo, title, bankInternationalPdf, footerPdf)
+
+  generatePdf(nameFile, logo, titleInternacional, bankInternationalPdf, footerPdf)
 }
 
 const copyToClipboard = (text: string) => {
@@ -316,11 +308,13 @@ const copyToClipboard = (text: string) => {
   border: 1px solid #dee2e6;
   border-width: 0 0 2px 0;
 }
+
 .p-tabmenu .p-tabmenu-nav .p-tabmenuitem.p-highlight .p-menuitem-link {
   background: transparent !important;
   border-color: var(--primary-color);
   color: var(--primary-color);
 }
+
 .p-tabmenu .p-tabmenu-nav .p-tabmenuitem .p-menuitem-link {
   border: solid #dee2e6;
   border-width: 0 0 2px 0;
@@ -334,6 +328,7 @@ const copyToClipboard = (text: string) => {
   transition: box-shadow 0.2s;
   margin: 0 0 -2px 0;
 }
+
 p {
   margin: 0 0 6px 0;
 }
@@ -341,6 +336,7 @@ p {
 .download-btn {
   width: 100% !important;
 }
+
 .grayed-text-warning {
   color: #6c757d !important;
   margin: 0 0 0 4px;
