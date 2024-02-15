4d067e6079a2074a47a2b2317ad84d138c209516
jlanza1981 <lanza.johanna@gmail.com>
2024-02-15 11:56:04 -0400

    correcion

--- src/views/withdraw/beneficiary/components/StepAccountNewBeneficiary.vue
+++ src/views/withdraw/beneficiary/components/StepAccountNewBeneficiary.vue
@@ -3,68 +3,70 @@
     <div class="field">
       <label>{{ t('bankName') }}</label>
       <div class="p-inputgroup">
-        <InputText type="text" v-model="formObject.informationBank.bankName" />
+        <InputText type="text" v-model="formObject.informationBank.bankName"/>
       </div>
     </div>
 
     <div class="field">
       <label>{{ t('accountNumber') }}</label>
       <div class="p-inputgroup">
-        <InputText type="text" v-model="formObject.informationBank.accountNumber" />
+        <InputText type="text" v-model="formObject.informationBank.accountNumber"/>
       </div>
     </div>
 
     <div class="field" v-if="typeBeneficiary !== 'DOMESTIC'">
       <label>{{ t('swiftCode') }}</label>
       <div class="p-inputgroup">
-        <InputText type="text" v-model="formObject.informationBank.swiftCode" />
+        <InputText type="text" v-model="formObject.informationBank.swiftCode"/>
       </div>
     </div>
 
     <div class="field">
       <label v-show="typeBeneficiary == 'DOMESTIC'">ABA Fedwire</label>
       <label v-show="typeBeneficiary == 'INTERNATIONAL'">{{ t('iban') }} </label>
-      <div class="p-inputgroup">
-        <InputText type="text" v-model="formObject.informationBank.routingNumber" />
+      <div class="p-inputgroup" v-if="typeBeneficiary == 'DOMESTIC'">
+        <InputText type="text" v-model="formObject.informationBank.routingNumber"/>
+      </div>
+      <div class="p-inputgroup" v-if="typeBeneficiary == 'INTERNATIONAL'">
+        <InputText type="text" v-model="formObject.informationBank.iban"/>
       </div>
     </div>
 
     <div class="field mt-5 flex justify-content-end">
-      <Button :label="t('nextButtonText')" class="px-5" @click="nextStep()" iconPos="right" />
+      <Button :label="t('nextButtonText')" class="px-5" @click="nextStep()" iconPos="right"/>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useI18n } from 'vue-i18n'
+import {ref} from 'vue'
+import {useI18n} from 'vue-i18n'
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
-import { useToast } from 'primevue/usetoast'
-import { useNewOrEditBeneficiary } from '../composable/useNewOrEditBeneficiary'
+import {useToast} from 'primevue/usetoast'
+import {useNewOrEditBeneficiary} from '../composable/useNewOrEditBeneficiary'
 
 const emit = defineEmits(['nextPage', 'prevPage'])
 
 const toast = useToast()
 
-const { t } = useI18n({ useScope: 'global' })
+const {t} = useI18n({useScope: 'global'})
 
-const { typeBeneficiary } = useNewOrEditBeneficiary()
+const {typeBeneficiary} = useNewOrEditBeneficiary()
 
 const routingNumberOrIBAN = ref<string>('')
 
-const { formObject } = useNewOrEditBeneficiary()
+const {formObject} = useNewOrEditBeneficiary()
 const validateFields = () => {
   let isSwiftCodeValid = true
   let isIbanValid = true
   let nameField = ''
   const isBankNameValid = formObject.value.informationBank.bankName.trim() !== ''
   let isAccountNumberValid = formObject.value.informationBank.accountNumber.trim() !== ''
-  isIbanValid = formObject.value.informationBank.routingNumber?.trim() !== ''
 
   if (typeBeneficiary.value === 'INTERNATIONAL') {
     isSwiftCodeValid = formObject.value.informationBank.swiftCode!.trim() !== ''
-
+    isIbanValid = formObject.value.informationBank.iban?.trim() !== ''
     if (isAccountNumberValid && !isIbanValid) {
       isIbanValid = true
     }
@@ -72,7 +74,7 @@ const validateFields = () => {
       isAccountNumberValid = true
     }
   }
-
+  console.log(isBankNameValid + '&&' + isAccountNumberValid + '&&' + isSwiftCodeValid + '&&' + isIbanValid)
   return isBankNameValid && isAccountNumberValid && isSwiftCodeValid && isIbanValid
 }
 
@@ -92,9 +94,10 @@ const nextStep = () => {
   formObject.value.informationBank.typeBeneficiaryBankWithdrawal = typeBeneficiary.value
   //Iban / Swift - International | Routing number Domestic
   if (typeBeneficiary.value === 'INTERNATIONAL') {
-    formObject.value.informationBank.iban = routingNumberOrIBAN.value
+    //formObject.value.informationBank.iban = routingNumberOrIBAN.value
+    delete formObject.value.informationBank.routingNumber
   } else if (typeBeneficiary.value === 'DOMESTIC') {
-    formObject.value.informationBank.routingNumber = routingNumberOrIBAN.value
+    //formObject.value.informationBank.routingNumber = routingNumberOrIBAN.value
     delete formObject.value.informationBank.iban
     delete formObject.value.informationBank.swiftCode
   }
