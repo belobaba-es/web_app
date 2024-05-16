15802713ca04d71fd98bc14147b4ae882fc463c8
abejarano <programador.angel@gmail.com>
2024-05-16 14:38:12 -0300

    fix: endpoint ForgotPasswordService

--- src/views/forgot-password/services/forgot-password.ts
+++ src/views/forgot-password/services/forgot-password.ts
@@ -2,6 +2,6 @@ import { HttpService } from '../../../shared/services/http'
 
 export class ForgotPasswordService {
   async sendEmail(email: string): Promise<any> {
-    return await new HttpService(import.meta.env.VITE_BASE_ENDPOINT).post(`/user/recovery-password`, { email }, false)
+    return await new HttpService(`${import.meta.env.VITE_BASE_ENDPOINT}/api/v1`).post(`/user/recovery-password`, { email }, false)
   }
 }
