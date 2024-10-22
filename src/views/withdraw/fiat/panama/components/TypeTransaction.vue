aea8dbf5af6bea4112c51526d54549ddeedc990f
jlanza1981 <lanza.johanna@gmail.com>
2024-10-22 16:43:18 -0400

    fix: se justaron las imagenes de publicidad

--- src/views/withdraw/fiat/panama/components/TypeTransaction.vue
+++ src/views/withdraw/fiat/panama/components/TypeTransaction.vue
@@ -44,23 +44,23 @@
       </div>
     </div>
     <div class="col-12 sm:col-12 md:col-5 lg:col-5 xl:col-5 line-vertical hidden md:block">
-      <!--      <div class="flex flex-column align-items-center pl-4">-->
-      <!--        <Button text @click="onClickLearnMore"-->
-      <!--          ><img-->
-      <!--            :src="`https://storage.googleapis.com/noba-dev/${t('imgPublicity.otc')}`"-->
-      <!--            alt="placeholder"-->
-      <!--            class="mt-3"-->
-      <!--            style="width: 300px; height: 150px; border-radius: 9px"-->
-      <!--        /></Button>-->
+      <div class="flex flex-column align-items-center pl-4">
+        <Button text @click="onClickLearnMore"
+          ><img
+            :src="`https://storage.googleapis.com/noba-dev/${t('imgPublicity.otc')}`"
+            alt="placeholder"
+            class="mt-3"
+            style="width: 300px; height: 150px; border-radius: 9px"
+        /></Button>
 
-      <!--        <Button text @click="onClickLearnMore"-->
-      <!--          ><img-->
-      <!--            :src="`https://storage.googleapis.com/noba-dev/${t('imgPublicity.help')}`"-->
-      <!--            alt="placeholder"-->
-      <!--            class="mt-3"-->
-      <!--            style="width: 300px; height: 150px; border-radius: 9px"-->
-      <!--        /></Button>-->
-      <!--      </div>-->
+        <Button text @click="onClickLearnMoreCovert"
+          ><img
+            :src="`https://storage.googleapis.com/noba-dev/${t('imgPublicity.convert')}`"
+            alt="placeholder"
+            class="mt-3"
+            style="width: 300px; height: 150px; border-radius: 9px"
+        /></Button>
+      </div>
     </div>
   </div>
 </template>
@@ -76,8 +76,22 @@ const toBack = () => {
 const newBeneficiaryPanama = () => {
   return router.push(`/withdraw/fiat/panama/new-beneficiary`)
 }
+const { locale } = useI18n()
 
-const onClickLearnMore = () => window.open('https://bit.ly/nobacash', '_blank')
+const onClickLearnMore = () =>
+  window.open(
+    locale.value == 'en'
+      ? 'https://belobaba.io/crypto-regulated-global-banking/khan-plus-en'
+      : 'https://belobaba.io/crypto-regulated-global-banking/khan-plus',
+    '_blank'
+  )
+const onClickLearnMoreCovert = () =>
+  window.open(
+    locale.value == 'en'
+      ? 'https://belobaba.io/crypto-regulated-global-banking/cuentas-en-eur-gbp-english'
+      : 'https://belobaba.io/crypto-regulated-global-banking/cuentas-eur-gbp',
+    '_blank'
+  )
 </script>
 <style lang="scss" scoped>
 .line-vertical {
--- src/views/withdraw/fiat/usa/components/WithdrawType.vue
+++ src/views/withdraw/fiat/usa/components/WithdrawType.vue
@@ -25,23 +25,23 @@
       </div>
     </div>
     <div class="col-12 sm:col-12 md:col-5 lg:col-5 xl:col-5 line-vertical hidden md:block">
-      <!--      <div class="flex flex-column align-items-center pl-4">-->
-      <!--        <Button text @click="onClickLearnMore"-->
-      <!--          ><img-->
-      <!--            :src="`https://storage.googleapis.com/noba-dev/${t('imgPublicity.otc')}`"-->
-      <!--            alt="placeholder"-->
-      <!--            class="mt-3"-->
-      <!--            style="width: 300px; height: 150px; border-radius: 9px"-->
-      <!--        /></Button>-->
+      <div class="flex flex-column align-items-center pl-4">
+        <Button text @click="onClickLearnMore"
+          ><img
+            :src="`https://storage.googleapis.com/noba-dev/${t('imgPublicity.otc')}`"
+            alt="placeholder"
+            class="mt-3"
+            style="width: 300px; height: 150px; border-radius: 9px"
+        /></Button>
 
-      <!--        <Button text @click="onClickLearnMore"-->
-      <!--          ><img-->
-      <!--            :src="`https://storage.googleapis.com/noba-dev/${t('imgPublicity.help')}`"-->
-      <!--            alt="placeholder"-->
-      <!--            class="mt-3"-->
-      <!--            style="width: 300px; height: 150px; border-radius: 9px"-->
-      <!--        /></Button>-->
-      <!--      </div>-->
+        <Button text @click="onClickLearnMoreCovert"
+          ><img
+            :src="`https://storage.googleapis.com/noba-dev/${t('imgPublicity.convert')}`"
+            alt="placeholder"
+            class="mt-3"
+            style="width: 300px; height: 150px; border-radius: 9px"
+        /></Button>
+      </div>
     </div>
   </div>
 </template>
@@ -56,7 +56,21 @@ import BackButtonMobile from '../../../../../components/BackButtonMobile.vue'
 
 const { t } = useI18n({ useScope: 'global' })
 const { showLabelInternational, showLabelDomestic } = useListBeneficiaryUsa()
-const onClickLearnMore = () => window.open('https://bit.ly/nobacash', '_blank')
+const { locale } = useI18n()
+const onClickLearnMore = () =>
+  window.open(
+    locale.value == 'en'
+      ? 'https://belobaba.io/crypto-regulated-global-banking/khan-plus-en'
+      : 'https://belobaba.io/crypto-regulated-global-banking/khan-plus',
+    '_blank'
+  )
+const onClickLearnMoreCovert = () =>
+  window.open(
+    locale.value == 'en'
+      ? 'https://belobaba.io/crypto-regulated-global-banking/cuentas-en-eur-gbp-english'
+      : 'https://belobaba.io/crypto-regulated-global-banking/cuentas-eur-gbp',
+    '_blank'
+  )
 const withdrawalSWIFT = ref(t('withdrawalSWIFT'))
 const typeWithdrawText = ref(t('typeWithdrawText'))
 const subtitle = ref('')
