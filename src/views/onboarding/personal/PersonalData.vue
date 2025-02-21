006ec2b07332e05ba37796b94161742d522be1ee
abejarano <programador.angel@gmail.com>
2025-02-21 17:53:07 -0300

    fix: showComboState

--- src/views/onboarding/personal/PersonalData.vue
+++ src/views/onboarding/personal/PersonalData.vue
@@ -3,14 +3,14 @@
     <div class="formgrid grid col-12">
       <div class="grid col-12">
         <div class="field col-12 sm:col-12 md:col-12 lg:col-4 xl:col-4">
-          <label>{{ t('nameLabel') }} <span class="bg-red" v-tooltip.top="'Mandatory'">*</span></label>
+          <label>{{ t('nameLabel') }} <span v-tooltip.top="'Mandatory'" class="bg-red">*</span></label>
           <div class="p-inputgroup">
             <InputText
-              type="text"
               v-model="onboardingPersonal.firstName"
               :value="onboardingPersonal.firstName"
               class="w-full"
               required
+              type="text"
             />
           </div>
           <div>
@@ -19,9 +19,9 @@
         </div>
 
         <div class="field col-12 sm:col-12 md:col-12 lg:col-4 xl:col-4">
-          <label>{{ t('secondNameLabel') }} <span class="bg-red" v-tooltip.top="'Mandatory'">*</span></label>
+          <label>{{ t('secondNameLabel') }} <span v-tooltip.top="'Mandatory'" class="bg-red">*</span></label>
           <div class="p-inputgroup">
-            <InputText type="text" v-model="onboardingPersonal.middleName" class="w-full" required />
+            <InputText v-model="onboardingPersonal.middleName" class="w-full" required type="text" />
           </div>
           <div>
             <span class="help-text">{{ t('helpTextMiddleName') }}</span>
@@ -29,9 +29,9 @@
         </div>
 
         <div class="field col-12 sm:col-12 md:col-12 lg:col-4 xl:col-4">
-          <label>{{ t('surnameLabel') }} <span class="bg-red" v-tooltip.top="'Mandatory'">*</span></label>
+          <label>{{ t('surnameLabel') }} <span v-tooltip.top="'Mandatory'" class="bg-red">*</span></label>
           <div class="p-inputgroup">
-            <InputText type="text" v-model="onboardingPersonal.lastName" class="w-full" required />
+            <InputText v-model="onboardingPersonal.lastName" class="w-full" required type="text" />
           </div>
           <div>
             <span class="help-text">{{ t('helpTextSurname') }}</span>
@@ -39,27 +39,27 @@
         </div>
 
         <div class="field col-12 sm:col-12 md:col-12 lg:col-2 xl:col-2">
-          <label>{{ t('birthdateLabel') }} <span class="bg-red" v-tooltip.top="'Mandatory'">*</span></label>
+          <label>{{ t('birthdateLabel') }} <span v-tooltip.top="'Mandatory'" class="bg-red">*</span></label>
           <div class="p-inputgroup">
             <InputMask id="basic" v-model="onboardingPersonal.dateBirth" mask="9999-99-99" slotChar="yyyy/mm/dd" />
           </div>
         </div>
 
         <div class="col-12 sm:col-12 md:col-12 lg:col-5 xl:col-5 phone-input">
-          <label>{{ t('phoneLabel') }} <span class="bg-red" v-tooltip.top="'Mandatory'">*</span></label>
+          <label>{{ t('phoneLabel') }} <span v-tooltip.top="'Mandatory'" class="bg-red">*</span></label>
           <div class="field grid">
             <div class="col-2">
               <Dropdown
-                class="w-full"
                 v-model="onboardingPersonal.phoneCountry"
+                :options="countries"
+                class="w-full"
                 filter
                 option-value="calling_code"
                 optionLabel="calling_code"
-                :options="countries"
               />
             </div>
             <div class="col-10">
-              <InputText id="phoneNumber" type="number" class="" v-model="onboardingPersonal.phoneNumber" required />
+              <InputText id="phoneNumber" v-model="onboardingPersonal.phoneNumber" class="" required type="number" />
               <div>
                 <span class="help-text">{{ t('helpTextPhone') }}</span>
               </div>
@@ -68,35 +68,35 @@
         </div>
 
         <div class="field col-12 sm:col-12 md:col-12 lg:col-5 xl:col-5">
-          <label>{{ t('emailLabel') }} <span class="bg-red" v-tooltip.top="'Mandatory'">*</span></label>
+          <label>{{ t('emailLabel') }} <span v-tooltip.top="'Mandatory'" class="bg-red">*</span></label>
           <div class="p-inputgroup">
             <!-- //FIX readonly -->
-            <InputText type="text" v-model="onboardingPersonal.email" class="w-full" readonly required />
+            <InputText v-model="onboardingPersonal.email" class="w-full" readonly required type="text" />
           </div>
         </div>
 
         <div class="flex flex-wrap gap-3 col-12 mb-5 mt-5 align-content-center">
-          <label>{{ t('validDocument') }} <span class="bg-red" v-tooltip.top="'Mandatory'">*</span></label>
+          <label>{{ t('validDocument') }} <span v-tooltip.top="'Mandatory'" class="bg-red">*</span></label>
 
           <div class="flex justify-content-center">
             <div class="flex gap-3">
               <div v-for="data in typeDocument" :key="data.name" class="flex align-items-center">
-                <RadioButton v-model="isHaveDocumentUS" :inputId="data.name" name="dynamic" :value="data.key" />
+                <RadioButton v-model="isHaveDocumentUS" :inputId="data.name" :value="data.key" name="dynamic" />
                 <label :for="data.name" class="ml-2">{{ data.name }}</label>
               </div>
             </div>
           </div>
         </div>
 
-        <div class="field col-12 sm:col-12 md:col-12 lg:col-4 xl:col-4" v-if="!isHaveDocumentUS">
+        <div v-if="!isHaveDocumentUS" class="field col-12 sm:col-12 md:col-12 lg:col-4 xl:col-4">
           <label>{{ t('docTypeLabelPassport') }}</label>
           <div class="p-inputgroup">
             <InputText
-              type="text"
-              v-tooltip.top="'not required'"
               v-model="onboardingPersonal.passport"
+              v-tooltip.top="'not required'"
               class="w-full"
               required
+              type="text"
             />
           </div>
           <div>
@@ -105,9 +105,9 @@
         </div>
 
         <div class="field col-12 sm:col-12 md:col-12 lg:col-4 xl:col-4">
-          <label>{{ t('documentLabel') }} <span class="bg-red" v-tooltip.top="'Mandatory'">*</span></label>
+          <label>{{ t('documentLabel') }} <span v-tooltip.top="'Mandatory'" class="bg-red">*</span></label>
           <div class="p-inputgroup">
-            <InputText type="text" v-model="onboardingPersonal.dni" class="w-full" required />
+            <InputText v-model="onboardingPersonal.dni" class="w-full" required type="text" />
           </div>
           <div>
             <span class="help-text">{{ t('helpTextIdNumber') }}</span>
@@ -115,9 +115,9 @@
         </div>
 
         <div class="field col-12 sm:col-12 md:col-12 lg:col-4 xl:col-4">
-          <label>{{ t('taxIdLabel') }} <span class="bg-red" v-tooltip.top="'Mandatory'">*</span></label>
+          <label>{{ t('taxIdLabel') }} <span v-tooltip.top="'Mandatory'" class="bg-red">*</span></label>
           <div class="p-inputgroup">
-            <InputText type="text" v-model="onboardingPersonal.taxId" class="w-full" required />
+            <InputText v-model="onboardingPersonal.taxId" class="w-full" required type="text" />
           </div>
           <div>
             <span class="help-text">{{ t('helpTextTaxIDNumber') }}</span>
@@ -132,16 +132,16 @@
         <Divider class="mt-0"></Divider>
         <div class="grid col-12 mt-2">
           <div class="field col-12 sm:col-12 md:col-12 lg:col-4 xl:col-4">
-            <label>{{ t('employmentStatus') }} <span class="bg-red" v-tooltip.top="'Mandatory'">*</span></label>
+            <label>{{ t('employmentStatus') }} <span v-tooltip.top="'Mandatory'" class="bg-red">*</span></label>
             <div class="p-inputgroup">
               <Dropdown
                 v-model="onboardingPersonal.employmentStatus"
                 :options="employmentStatusList"
-                optionLabel="name"
-                option-value="value"
-                filter
                 :placeholder="t('employmentStatusPlaceholder')"
                 class="w-full"
+                filter
+                option-value="value"
+                optionLabel="name"
                 required
               />
             </div>
@@ -151,9 +151,9 @@
           </div>
 
           <div class="field col-12 sm:col-12 md:col-12 lg:col-4 xl:col-4">
-            <label>{{ t('occupationLabel') }} <span class="bg-red" v-tooltip.top="'Mandatory'">*</span></label>
+            <label>{{ t('occupationLabel') }} <span v-tooltip.top="'Mandatory'" class="bg-red">*</span></label>
             <div class="p-inputgroup">
-              <InputText type="text" v-model="onboardingPersonal.occupation" class="w-full" />
+              <InputText v-model="onboardingPersonal.occupation" class="w-full" type="text" />
             </div>
             <div>
               <span class="help-text">{{ t('helpTextOccupation') }}</span>
@@ -166,19 +166,19 @@
       <Divider class="mt-0"></Divider>
       <div class="grid mt-2">
         <div class="field col-12 sm:col-12 md:col-12 lg:col-4 xl:col-4">
-          <label>{{ t('countryLabel') }} <span class="bg-red" v-tooltip.top="'Mandatory'">*</span></label>
+          <label>{{ t('countryLabel') }} <span v-tooltip.top="'Mandatory'" class="bg-red">*</span></label>
           <div class="p-inputgroup">
             <Dropdown
               v-model="onboardingPersonal.country"
-              :options="countries"
-              optionLabel="name"
-              option-value="country_code"
-              filter
               :loading="loadingCountriesField"
+              :options="countries"
               :placeholder="t('countryPlaceholder')"
               class="w-full"
-              @change="onChangeCountryHandler"
+              filter
+              option-value="country_code"
+              optionLabel="name"
               required
+              @change="changeCountryHandler"
             />
           </div>
           <div>
@@ -187,9 +187,23 @@
         </div>
 
         <div class="field col-12 sm:col-12 md:col-12 lg:col-4 xl:col-4">
-          <label>{{ t('stateLabel') }} <span class="bg-red" v-tooltip.top="'Mandatory'">*</span></label>
-          <div class="p-inputgroup">
-            <InputText type="text" v-model="onboardingPersonal.region" class="w-full" />
+          <label>{{ t('stateLabel') }} <span v-tooltip.top="'Mandatory'" class="bg-red">*</span></label>
+          <div v-if="!showComboState" class="p-inputgroup">
+            <InputText v-model="onboardingPersonal.region" class="w-full" type="text" />
+          </div>
+          <div v-if="showComboState" class="p-inputgroup">
+            <div class="p-inputgroup">
+              <Dropdown
+                v-model="onboardingPersonal.region"
+                :options="state_us"
+                :placeholder="t('stateLabel')"
+                class="w-full"
+                filter
+                option-value="state_code"
+                optionLabel="name"
+                required
+              />
+            </div>
           </div>
           <div>
             <span class="help-text">{{ t('helpTextState') }}</span>
@@ -197,9 +211,9 @@
         </div>
 
         <div class="field col-12 sm:col-12 md:col-12 lg:col-4 xl:col-4">
-          <label>{{ t('cityLabel') }} <span class="bg-red" v-tooltip.top="'Mandatory'">*</span></label>
+          <label>{{ t('cityLabel') }} <span v-tooltip.top="'Mandatory'" class="bg-red">*</span></label>
           <div class="p-inputgroup">
-            <InputText type="text" v-model="onboardingPersonal.city" class="w-full" required />
+            <InputText v-model="onboardingPersonal.city" class="w-full" required type="text" />
           </div>
           <div>
             <span class="help-text">{{ t('helpTextCity') }}</span>
@@ -207,9 +221,9 @@
         </div>
 
         <div class="field col-12 sm:col-12 md:col-12 lg:col-4 xl:col-4">
-          <label>{{ t('streetAddress') }} <span class="bg-red" v-tooltip.top="'Mandatory'">*</span></label>
+          <label>{{ t('streetAddress') }} <span v-tooltip.top="'Mandatory'" class="bg-red">*</span></label>
           <div class="p-inputgroup">
-            <InputText type="text" v-model="onboardingPersonal.streetOne" />
+            <InputText v-model="onboardingPersonal.streetOne" type="text" />
           </div>
           <div>
             <span class="help-text">{{ t('helpTextAddressOne') }}</span>
@@ -219,7 +233,7 @@
         <div class="field col-12 sm:col-12 md:col-12 lg:col-4 xl:col-4">
           <label>{{ t('streetAddressTwo') }}</label>
           <div class="p-inputgroup">
-            <InputText type="text" v-model="onboardingPersonal.streetTwo" />
+            <InputText v-model="onboardingPersonal.streetTwo" type="text" />
           </div>
           <div>
             <span class="help-text">{{ t('helpTextAddressTwo') }}</span>
@@ -227,9 +241,9 @@
         </div>
 
         <div class="field col-12 sm:col-12 md:col-12 lg:col-4 xl:col-4">
-          <label>{{ t('postalCodeLabel') }} <span class="bg-red" v-tooltip.top="'Mandatory'">*</span></label>
+          <label>{{ t('postalCodeLabel') }} <span v-tooltip.top="'Mandatory'" class="bg-red">*</span></label>
           <div class="p-inputgroup">
-            <InputText type="text" v-model="onboardingPersonal.postalCode" />
+            <InputText v-model="onboardingPersonal.postalCode" type="text" />
           </div>
           <div>
             <span class="help-text">{{ t('helpTextAddressPostalCode') }}</span>
@@ -238,12 +252,12 @@
       </div>
 
       <div class="field col-12 flex align-items-center justify-content-end">
-        <router-link to="/onboarding/personal/investment-data" custom v-slot="{ navigate }">
+        <router-link v-slot="{ navigate }" custom to="/onboarding/personal/investment-data">
           <Button
             :label="t('nextButtonText')"
+            :loading="submitting"
             class="px-5 mt-2 btn-submit"
             @click="saveData(navigate)"
-            :loading="submitting"
           />
         </router-link>
       </div>
@@ -252,8 +266,8 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue'
-import Dropdown from 'primevue/dropdown'
+import { onMounted, ref, watch } from 'vue'
+import Dropdown, { DropdownChangeEvent } from 'primevue/dropdown'
 import InputText from 'primevue/inputtext'
 import Divider from 'primevue/divider'
 import InputMask from 'primevue/inputmask'
@@ -266,12 +280,25 @@ import { useOnboardingPersonal } from '../../../composables/useOnboardingPersona
 import { useEmployment } from '../../../composables/useEmployment'
 import Button from 'primevue/button'
 
-const { countries, fetchCountries, loadingCountriesField, onChangeCountryHandler } = useWorld()
+const { countries, fetchCountries, loadingCountriesField, onChangeCountryHandler, state_us } = useWorld()
 
 const { employmentStatusList } = useEmployment()
 
 const { onboardingPersonal, submitting, saveData, typeDocument, isHaveDocumentUS } = useOnboardingPersonal()
 const { t } = useI18n({ useScope: 'global' })
+
+const showComboState = ref<boolean>(false)
+
+const changeCountryHandler = async (event: DropdownChangeEvent) => {
+  onChangeCountryHandler(event, showComboState)
+}
+
+watch(
+  () => onboardingPersonal.value.country,
+  newValue => {
+    showComboState.value = newValue === 'US'
+  }
+)
 
 onMounted(async () => {
   await fetchCountries()
